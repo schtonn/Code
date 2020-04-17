99db201bf07743234130b0ffd996b449ca89a12d
schtonn <m18519511495@163.com>
2020-04-17 20:53:25 +0800

    add dimension

--- test.cpp
+++ test.cpp
@@ -1,31 +1,42 @@
 #include"iostream"
 using namespace std;
-int t[500010],n,m,op,x,y;
+int t[1034][1034],n,m,op,l,r,x,y,c;
 int lowbit(int x){return x&-x;}
-void add(int i,int x){
-	while(i<=n){
-		t[i]+=x;
+void add(int i,int j,int x){
+	while(i<n){
+		while(j<n){
+			t[i][j]+=x;
+			j+=lowbit(j);
+		}
 		i+=lowbit(i);
 	}
 }
-int query(int i){
+int query(int i,int j){
 	int sum=0;
-	while(i>0){
-		sum+=t[i];
+	while(i){
+		while(j){
+			sum+=t[i][j];
+			j-=lowbit(j);
+		}
 		i-=lowbit(i);
 	}
 	return sum;
 }
 int main(){
-	cin>>n>>m;
-	for(int i=1;i<=n;i++){
+	while(true){
 		cin>>op;
-		add(i,op);
-	}
-	for(int i=1;i<=m;i++){
-		cin>>op>>x>>y;
-		if(op==1)add(x,y);
-		else cout<<query(y)-query(x-1)<<endl;
+		if(op==0){
+			cin>>n;
+		}else if(op==1){
+			cin>>x>>y>>c;
+			add(x,y,c);
+		}
+		else if(op==2){
+			cin>>l>>x>>r>>y;
+			cout<<query(r,y)-query(r,x)-query(l,y)+query(l,x)<<endl;
+		}else{
+			break;
+		}
 	}
 	return 0;
 }
