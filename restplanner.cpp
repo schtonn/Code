45df6c7e1aa57646fd11b450a70474e2a0a23565
schtonn <m18519511495@163.com>
2020-04-18 23:20:12 +0800

    check done

--- restplanner.cpp
+++ restplanner.cpp
@@ -1,15 +1,32 @@
-#include "bits/stdc++.h"
+#include"bits/stdc++.h"
 using namespace std;
+int a[200],b[200],n,m,op,x,y;
 int main(){
-	freopen("hamar.doc","w",stdout);
-	srand(time(NULL));
-	for(int i=1;i<=300;i++){
-		int a=100,b=0;
-		while(a-b>9){
-			a=rand()%10+10;
-			b=rand()%9+1;
+	freopen("testdata.in","r",stdin);
+	freopen("ghostdata.out","w",stdout);
+	cin>>n>>m;
+	for(int i=1;i<=n;i++){
+		cin>>a[i]>>b[i];
+	}
+	for(int i=1;i<=m;i++){
+		cin>>op>>x>>y;
+		if(op==1){
+			for(int j=x;j<=y;j++){
+				a[j]=a[j]+b[j];
+			}
 		}
-		cout<<a<<"-"<<b<<"="<<endl;
+		else if(op==2){
+			for(int j=x;j<=y;j++){
+				b[j]=a[j]+b[j];
+			}
+		}
+		else{
+			int sum=0;
+			for(int j=x;j<=y;j++){
+				sum=(sum+10301+(a[j]*b[j])%10301)%10301;
+			}
+			cout<<sum<<endl;
+		}
 	}
 	return 0;
 }
