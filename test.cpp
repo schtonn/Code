60810e73ac11db59821ebaf0c51b3b585cec0fd0
schtonn <m18519511495@163.com>
2020-04-21 20:18:57 +0800

    div 2

--- test.cpp
+++ test.cpp
@@ -1,20 +1,63 @@
 #include "bits/stdc++.h"
 using namespace std;
-int price[10000],n,b,k,sum;
+vector <string>clip(200);
+int n,T;
 int main(){
 	cin>>n;
-	for(int i=1;i<=n;i++){
-		cin>>price[i];
+	for(int i=0;i<n;i++){
+		cin>>clip[i];
 	}
-	cin>>b>>k;
-	sort(price+1,price+1+n);
-	for(int i=n;i>n-k;i--){
-		sum+=price[i];
-		if(sum>b){
-			cout<<"NO"<<endl;
-			return 0;
+	cin>>T;
+	int x=n;
+	int y=clip[0].size();
+	int s=-(y-1);
+	int t=(x-1);
+	long long cnt[200];
+	for(int i=0;i<200;i++)
+		cnt[i]=0;
+	long long count_black=0;
+	for(int i=s;i<=0;i++){
+		int j=0;
+		int k=j-i;
+		bool fs=false;
+		int ft=-1;
+		for(j=0;j<x;j++){
+			k=j-i;
+			if(k>=y)break;
+			if(clip[j][k]=='B'){
+				if(fs==false){
+					ft=j+T;
+					cnt[i+y]+=T;
+				}
+				fs=true;
+				cnt[i+y]+=min((j+T)-ft,T);
+				ft=j+T;
+			}
 		}
 	}
-	cout<<"YES"<<endl;
+	for(int i=1;i<=t;i++){
+		int j=0;
+		int k=j+i;
+		bool fs=false;
+		int ft=-1;
+		for(j=0;j<y;j++){
+			k=j+i;
+			if(k>=x)
+				break;
+			if(clip[k][j]=='B'){
+				if(fs==false){
+					ft=k+T;
+					cnt[i+y]+=T;
+				}
+				fs=true;
+				cnt[i+y]+=min((k+T)-ft,T);
+				ft=k+T;
+			}
+		}
+	}
+	for(int i=0;i<200;i++){
+		count_black+=cnt[i];
+	}
+	cout<<count_black<<endl;
 	return 0;
 }
