373d694cb6141554ae259aade8034eca0a8154b3
schtonn <m18519511495@163.com>
2020-04-21 20:26:58 +0800

    div 3

--- test.cpp
+++ test.cpp
@@ -12,10 +12,13 @@ int main(){
 	int y=clip[0].size();
 	int s=-(y-1);
 	int t=(x-1);
-	long long cnt[200];
-	for(int i=0;i<200;i++)
-		cnt[i]=0;
-	long long count_black=0;
+	long long cnt_r[200];
+	long long cnt_g[200];
+	long long cnt_b[200];
+	for(int i=0;i<200;i++)cnt_r[i]=cnt_g[i]=cnt_b[i]=0;
+	long long count_red=0;
+	long long count_green=0;
+	long long count_blue=0;
 	for(int i=s;i<=0;i++){
 		int j=0;
 		int k=j-i;
@@ -24,13 +27,31 @@ int main(){
 		for(j=0;j<x;j++){
 			k=j-i;
 			if(k>=y)break;
+			if(clip[j][k]=='R'){
+				if(fs==false){
+					ft=j+T;
+					cnt_r[i+y]+=T;
+				}
+				fs=true;
+				cnt_r[i+y]+=min((j+T)-ft,T);
+				ft=j+T;
+			}
+			if(clip[j][k]=='G'){
+				if(fs==false){
+					ft=j+T;
+					cnt_g[i+y]+=T;
+				}
+				fs=true;
+				cnt_g[i+y]+=min((j+T)-ft,T);
+				ft=j+T;
+			}
 			if(clip[j][k]=='B'){
 				if(fs==false){
 					ft=j+T;
-					cnt[i+y]+=T;
+					cnt_b[i+y]+=T;
 				}
 				fs=true;
-				cnt[i+y]+=min((j+T)-ft,T);
+				cnt_b[i+y]+=min((j+T)-ft,T);
 				ft=j+T;
 			}
 		}
@@ -44,20 +65,44 @@ int main(){
 			k=j+i;
 			if(k>=x)
 				break;
+			if(clip[k][j]=='R'){
+				if(fs==false){
+					ft=k+T;
+					cnt_r[i+y]+=T;
+				}
+				fs=true;
+				cnt_r[i+y]+=min((k+T)-ft,T);
+				ft=k+T;
+			}
+			if(clip[k][j]=='G'){
+				if(fs==false){
+					ft=k+T;
+					cnt_g[i+y]+=T;
+				}
+				fs=true;
+				cnt_g[i+y]+=min((k+T)-ft,T);
+				ft=k+T;
+			}
 			if(clip[k][j]=='B'){
 				if(fs==false){
 					ft=k+T;
-					cnt[i+y]+=T;
+					cnt_b[i+y]+=T;
 				}
 				fs=true;
-				cnt[i+y]+=min((k+T)-ft,T);
+				cnt_b[i+y]+=min((k+T)-ft,T);
 				ft=k+T;
 			}
 		}
 	}
 	for(int i=0;i<200;i++){
-		count_black+=cnt[i];
+		count_red+=cnt_r[i];
 	}
-	cout<<count_black<<endl;
+	for(int i=0;i<200;i++){
+		count_green+=cnt_g[i];
+	}
+	for(int i=0;i<200;i++){
+		count_blue+=cnt_b[i];
+	}
+	cout<<count_red<<' '<<count_green<<' '<<count_blue<<endl;
 	return 0;
 }
