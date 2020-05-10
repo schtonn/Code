f6bc639f633e2002dd1dadf6fa5bf38919933fd1
schtonn <m18519511495@163.com>
2020-05-10 19:41:58 +0800

    ans

--- calculate.cpp
+++ calculate.cpp
@@ -1,23 +1,36 @@
 #include "bits/stdc++.h"
 using namespace std;
 char x[100],y[100],z[100];
+int len;
 int main(){
     cin>>x>>y;
-    for(int i=0;i<=strlen(x);i++){
+	len=strlen(x);
+    for(int i=0;i<=len;i++){
         // cout<<i<<endl;
         for(int j=0;j<i;j++){
             z[j]=x[j];
         }
         // cout<<x<<endl<<y<<endl<<z<<endl;
-        for(int j=i;j<strlen(x)+i;j++){
+        for(int j=i;j<len+i;j++){
             z[j]=x[j-i];
         }
         // cout<<x<<endl<<y<<endl<<z<<endl;
-        for(int j=strlen(x)+i;j<strlen(x)*2;j++){
-            z[j]=x[j-strlen(x)];
+        for(int j=len+i;j<len*2;j++){
+            z[j]=x[j-len];
         }
         // cout<<x<<endl<<y<<endl<<z<<endl;
+		bool fleg=1;
+		for(int j=0;j<len*2;j++){
+			if(z[i]!=y[i]){
+				fleg=0;
+				break;
+			}
+		}
+		if(fleg==1){
+			cout<<"Yes"<<endl;
+			return 0;
+		}
     }
-    system("pause");
+	cout<<"No"<<endl;
     return 0;
 }
