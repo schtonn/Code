bafda3ff76483672b4c0ccbc170f16cb576f3d77
schtonn <m18519511495@163.com>
2020-05-10 19:23:51 +0800

    test dev and tomato

--- calculate.cpp
+++ calculate.cpp
@@ -1,181 +1,23 @@
-#include"iostream"
+#include "bits/stdc++.h"
 using namespace std;
-#define N 1010
-#define lson id<<1
-#define rson id<<1|1
-#define mod 10301
-int a[N],b[N],n,m,op,x,y,k;
-int base1[3][3]={
-    {1,1,2},
-    {0,1,0},
-    {0,1,1}};
-int base2[3][3]={
-    {1,0,0},
-    {1,1,2},
-    {1,0,1}};
-struct matrix{
-    int v[3][3];
-    int x,y;
-}c1,c2;
-struct node{
-    int l,r;
-    matrix sum,lazy;
-}t[4*N];
-ostream&operator<<(ostream&ous,matrix a){
-    for(int i=0;i<a.x;i++){
-        for(int j=0;j<a.y;j++){
-            ous<<a.v[i][j]<<' ';
-        }
-        ous<<endl;
-    }
-    return ous;
-}
-bool check(matrix a){
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if((i==j&&a.v[i][j]!=1)||(i!=j&&a.v[i][j]!=0))return 1;
-        }
-    }
-    return 0;
-}
-void clear(matrix&a){
-    for(int i=0;i<a.x;i++){
-        for(int j=0;j<a.y;j++){
-            if(i==j)a.v[i][j]=1;
-            else a.v[i][j]=0;
-        }
-    }
-}
-matrix operator+(matrix a,matrix b){
-    matrix c;
-    if(a.x!=b.x||a.y!=b.y)return c;
-    int x=a.x,y=a.y;
-    c.x=x;
-    c.y=y;
-    for(int i=0;i<x;i++){
-        for(int j=0;j<y;j++){
-            c.v[i][j]=(a.v[i][j]+b.v[i][j])%mod;
-        }
-    }
-    return c;
-}
-matrix operator*(matrix a,matrix b){
-    matrix c;
-    if(a.y!=b.x)return c;
-    int x=a.x,y=b.y,z=a.y;
-    c.x=x;
-    c.y=y;
-    for(int i=0;i<x;i++){
-        for(int j=0;j<y;j++){
-            c.v[i][j]=0;
-            for(int k=0;k<z;k++){
-                c.v[i][j]=(c.v[i][j]+a.v[i][k]*b.v[k][j]+mod)%mod;
-            }
-        }
-    }
-    return c;
-}
-matrix&operator*=(matrix&a,matrix n){
-    a=a*n;
-    return a;
-}
-void update(int id){
-    t[id].sum=(t[lson].lazy*t[lson].sum)+(t[rson].lazy*t[rson].sum);
-}
-void pushdown(int id){
-    if(check(t[id].lazy)){
-        t[lson].lazy=t[id].lazy*t[lson].lazy;
-        t[rson].lazy=t[id].lazy*t[rson].lazy;
-        t[id].sum=t[id].lazy*t[id].sum;
-        clear(t[id].lazy);
-    }
-}
-void buildtree(int id,int l,int r){
-    t[id].l=l;
-    t[id].r=r;
-    t[id].lazy.x=t[id].lazy.y=3;
-    clear(t[id].lazy);
-    t[id].sum.x=3;
-    t[id].sum.y=1;
-    if(l==r){
-        t[id].sum.v[0][0]=(a[l]*a[l])%mod;
-        t[id].sum.v[1][0]=(b[l]*b[l])%mod;
-        t[id].sum.v[2][0]=(a[l]*b[l])%mod;
-        return;
-    }
-    int mid=(l+r)>>1;
-    buildtree(lson,l,mid);
-    buildtree(rson,mid+1,r);
-    update(id);
-}
-void change1(int id,int l,int r){
-    if(t[id].l>=l&&t[id].r<=r){
-        t[id].lazy=c1*t[id].lazy;
-        return;
-    }
-    pushdown(id);
-    if(t[lson].r>=r)change1(lson,l,r);
-    else if(t[rson].l<=l)change1(rson,l,r);
-    else{
-        change1(lson,l,t[lson].r);
-        change1(rson,t[rson].l,r);
-    }
-    update(id);
-}
-void change2(int id,int l,int r){
-    if(t[id].l>=l&&t[id].r<=r){
-        t[id].lazy=c2*t[id].lazy;
-        return;
-    }
-    pushdown(id);
-    if(t[lson].r>=r)change2(lson,l,r);
-    else if(t[rson].l<=l)change2(rson,l,r);
-    else{
-        change2(lson,l,t[lson].r);
-        change2(rson,t[rson].l,r);
-    }
-    update(id);
-}
-matrix query(int id,int l,int r){
-    if(t[id].l>=l&&t[id].r<=r){
-        return t[id].lazy*t[id].sum;
-    }
-    pushdown(id);
-    if(t[lson].r>=r)return query(lson,l,r);
-    else if(t[rson].l<=l)return query(rson,l,r);
-    else{
-        return query(lson,l,t[lson].r)+query(rson,t[rson].l,r);
-    }
-}
-void init(){
-    for(int i=0;i<c1.x;i++){
-        for(int j=0;j<c1.y;j++){
-            c1.v[i][j]=base1[i][j];
-        }
-    }
-    for(int i=0;i<c2.x;i++){
-        for(int j=0;j<c2.y;j++){
-            c2.v[i][j]=base2[i][j];
-        }
-    }
-}
+char x[100],y[100],z[100];
 int main(){
-    freopen("testdata.in","r",stdin);
-    freopen("testdata.out","w",stdout);
-    c1.x=c1.y=c2.x=c2.y=3;
-    init();
-    cin>>n>>m;
-    for(int i=1;i<=n;i++){
-        cin>>a[i]>>b[i];
-        a[i]%=mod;
-        b[i]%=mod;
+    cin>>x>>y;
+    for(int i=0;i<=strlen(x);i++){
+        // cout<<i<<endl;
+        for(int j=0;j<i;j++){
+            z[j]=x[j];
+        }
+        // cout<<x<<endl<<y<<endl<<z<<endl;
+        for(int j=i;j<strlen(x)+i;j++){
+            z[j]=x[j-i];
+        }
+        // cout<<x<<endl<<y<<endl<<z<<endl;
+        for(int j=strlen(x)+i;j<strlen(x)*2;j++){
+            z[j]=x[j-strlen(x)];
+        }
+        // cout<<x<<endl<<y<<endl<<z<<endl;
     }
-    buildtree(1,1,n);
-    for(int i=1;i<=m;i++){
-        cin>>op>>x>>y;
-        if(op==1)change1(1,x,y);
-        else if(op==2)change2(1,x,y);
-        else cout<<query(1,x,y).v[2][0]%mod<<endl;
-    }
+    system("pause");
     return 0;
 }
--- restplanner.cpp
+++ restplanner.cpp
@@ -1,32 +1,14 @@
 #include"bits/stdc++.h"
+#include"windows.h"
 using namespace std;
-int a[200],b[200],n,m,op,x,y;
 int main(){
-    freopen("testdata.in","r",stdin);
-    freopen("ghostdata.out","w",stdout);
-    cin>>n>>m;
-    for(int i=1;i<=n;i++){
-        cin>>a[i]>>b[i];
-    }
-    for(int i=1;i<=m;i++){
-        cin>>op>>x>>y;
-        if(op==1){
-            for(int j=x;j<=y;j++){
-                a[j]=a[j]+b[j];
-            }
-        }
-        else if(op==2){
-            for(int j=x;j<=y;j++){
-                b[j]=a[j]+b[j];
-            }
-        }
-        else{
-            int sum=0;
-            for(int j=x;j<=y;j++){
-                sum=(sum+10301+(a[j]*b[j])%10301)%10301;
-            }
-            cout<<sum<<endl;
-        }
+    while(true){
+        Beep(500,1000);
+        cout<<"startwork"<<endl;
+        Sleep(25*60*1000);
+        Beep(700,1000);
+        cout<<"rest"<<endl;
+        Sleep(5*60*1000);
     }
     return 0;
 }
