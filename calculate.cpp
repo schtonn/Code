bb9bc79d81ce724ae668b93b5b71230dca6e54c2
schtonn <m18519511495@163.com>
2020-05-16 11:20:37 +0800

    coinsgameeasy

--- calculate.cpp
+++ calculate.cpp
@@ -1,98 +1,96 @@
 #include "bits/stdc++.h"
-
 using namespace std;
-
-
-int row,col;
-int ans;
-
-
-void dfs(vector<string> board,int step){
-    int dir_row[]={0,0,1,-1};
-    int dir_col[]={1,-1,0,0};
-    if(step>10 || step>=ans)return;
-    int r1=-1,r2=-1,c1=-1,c2=-1;
-    int r1_new=-1,r2_new=-1,c1_new=-1,c2_new=-1;
-    for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
-            if(board[i][j]=='o' && r1==-1){
-                r1=i; c1=j;
+int x,y,ans;
+int dirx[]={0,0,1,-1};
+int diry[]={1,-1,0,0};
+void theFatUglyLargeWizardishDepthFirstSearch(vector<string> chess,int step){
+    if(step>10||step>=ans)return;
+    int ax=-1,bx=-1,ay=-1,by=-1;
+    int axN=-1,bxN=-1,ayN=-1,byN=-1;
+    for(int i=0;i<x;i++){
+        for(int j=0;j<y;j++){
+            if(chess[i][j]=='o'&&ax==-1){
+                ax=i;ay=j;
                 continue;
             }
-            else if(board[i][j]=='o' && r1!=-1){
-                r2=i; c2=j;
+            else if(chess[i][j]=='o'&&ax!=-1){
+                bx=i;by=j;
                 break;
             }
         }
     }
     for(int i=0;i<4;i++){
-        int tmp_r=r1+dir_row[i];
-        int tmp_c=c1+dir_col[i];
+        int tmp_r=ax+dirx[i];
+        int tmp_c=ay+diry[i];
         bool moved=false;
         int counter=0;
-        if(tmp_r<0 || tmp_r>=row || tmp_c<0 || tmp_c>=col ){
+        if(tmp_r<0||tmp_r>=x||tmp_c<0||tmp_c>=y){
             counter++;
         }
-        tmp_r=r2+dir_row[i];
-        tmp_c=c2+dir_col[i];
-        if(tmp_r<0 || tmp_r>=row || tmp_c<0 || tmp_c>=col ){
+        tmp_r=bx+dirx[i];
+        tmp_c=by+diry[i];
+        if(tmp_r<0||tmp_r>=x||tmp_c<0||tmp_c>=y){
             counter++;
         }
         if(counter==1){
-            if(step<ans)
-                ans=step;
+            if(step<ans)ans=step;
             return;
         }
-        if(counter==2)
-            continue;
-
-        tmp_r=r1+dir_row[i];
-        tmp_c=c1+dir_col[i];
-        if(tmp_r>=0 && tmp_r<row && tmp_c>=0 && tmp_c<col 
-            && board[tmp_r][tmp_c]!='#'){
-                r1_new=tmp_r; c1_new=tmp_c; moved=true;
+        if(counter==2)continue;
+        tmp_r=ax+dirx[i];
+        tmp_c=ay+diry[i];
+        if(tmp_r>=0&&tmp_r<x&&tmp_c>=0&&tmp_c<y&&chess[tmp_r][tmp_c]!='#'){
+            axN=tmp_r;
+            ayN=tmp_c;
+            moved=true;
         }
         else{
-            r1_new=r1; c1_new=c1;
+            axN=ax;
+            ayN=ay;
         }
-
-        tmp_r=r2+dir_row[i];
-        tmp_c=c2+dir_col[i];
-        if(tmp_r>=0 || tmp_r<row || tmp_c>=0 || tmp_c<col ){
+        tmp_r=bx+dirx[i];
+        tmp_c=by+diry[i];
+        if(tmp_r>=0||tmp_r<x||tmp_c>=0||tmp_c<y){
             counter++;
         }
-        if(tmp_r>=0 && tmp_r<row && tmp_c>=0 && tmp_c<col 
-            && board[tmp_r][tmp_c]!='#'){
-                r2_new=tmp_r; c2_new=tmp_c; moved=true;
+        if(tmp_r>=0&&tmp_r<x&&tmp_c>=0&&tmp_c<y&&chess[tmp_r][tmp_c]!='#'){
+            bxN=tmp_r;
+            byN=tmp_c;
+            moved=true;
         }
         else{
-            r2_new=r2; c2_new=c2;
+            bxN=bx;
+            byN=by;
         }
-        
-        if(r1_new==r2_new && c1_new==c2_new)
-            continue;
-        if(moved==false)
-            continue;
-        vector<string> boa=board;
-        boa[r1][c1]='.'; boa[r2][c2]='.';
-        boa[r1_new][c1_new]='o'; boa[r2_new][c2_new]='o';
-        dfs(boa,step+1);
+        if(axN==bxN&&ayN==byN)continue;
+        if(moved==false)continue;
+        vector<string>chN=chess;
+        chN[ax][ay]='.';
+        chN[bx][by]='.';
+        chN[axN][ayN]='o';
+        chN[bxN][byN]='o';
+        theFatUglyLargeWizardishDepthFirstSearch(chN,step+1);
     }
 }
 
-int minimalSteps(vector <string> board) {	
-    row=board.size();
-    col=board[0].size();
+int theExterierOfTheFatUglyLargeWizardishDepthFirstSearch(vector <string> chess) {	
+    x=chess.size();
+    y=chess[0].size();
     ans=50;
-    dfs(board,1);
-    if(ans>10)
-        return -1;
+    theFatUglyLargeWizardishDepthFirstSearch(chess,1);
+    if(ans>10)return -1;
     return ans;
 }
-vector<string>board;
+vector<string>chess;
 int main(){
-    cin>>row>>col;
-    for(int i=0;i<row;i++){
-        cin>>board[i];
+    freopen("CoinsGameEasy.in","r",stdin);
+    freopen("CoinsGameEasy.out","w",stdout);
+    cin>>x>>y;
+    string theSmartTempOfInput;
+    for(int i=0;i<x;i++){
+        cin>>theSmartTempOfInput;
+        chess.push_back(theSmartTempOfInput);
     }
+    cout<<theExterierOfTheFatUglyLargeWizardishDepthFirstSearch(chess)<<endl;
+    system("pause");
 }
