7737ddcab17323de582c029afa6c3410b831aa9d
schtonn <m18519511495@163.com>
2020-05-31 19:50:11 +0800

    hamilton v4

--- test.cpp
+++ test.cpp
@@ -1,36 +1,44 @@
 #include "bits/stdc++.h"
-#include "iostream"
 using namespace std;
 const int N=1<<10;
-int n,m,e[25][25];
-bool dp[N][25];//dp[i][j]表示状态为i，最后到达j点有没有路径
-int g[N][25];
-int fact(int n) 
-{ if(n<2)                       //将 2 换成其它数如 8 就可输出 8 进制的结果
- return n; 
- else
-  { return fact(n/2)*10+n%2;                     //将二进制结果整个输出 
-  } 
-} 
+int n,m,g[N][25];
+int e[25][25];
+int dp[N][25];//dp[i][j]表示状态为i，最后到达j点有没有路径
+int len[N][25];
+int special[N][3];
+int fact(int n) {
+    if(n<2)return n;
+    else{
+        return fact(n/2)*10+n%2;
+    }
+}
 
 int main(){
-    cin>>n>>m;
-    for(int i=1;i<=m;i++){
-        int u,v;
-        cin>>u>>v;
-        e[u][v]=true;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            int c;
+            cin>>c;
+            e[i][j]=c;
+        }
     }
-    dp[1][1]=true;
+    for(int i=1;i<=n;i++){
+        dp[1<<(i-1)][i]=1;
+        special[1<<(i-1)][1]=-1;
+        special[1<<(i-1)][2]=i;
+    }
     for(int s=1;s<(1<<n);++s){
         for(int p=1;p<=n;++p){
             if(s&(1<<(p-1))==0)continue;//判断p是否为S中元素
             for(int q=1;q<=n;++q){
                 if(s&(1<<(q-1))==0)continue;//判断q是否为S中元素
-                if(!e[q][p])continue;//判断有没有q到p的边
+                if(e[q][p]==-1)continue;//判断有没有q到p的边
                 if(dp[s^(1<<(p-1))][q]){//判断dp[{s}-p][q]是否为真
+                    if(len[s][p]==0)len[s][p]=len[s^(1<<(p-1))][q]+e[q][p];
+                    else len[s][p]=min(len[s][p],len[s^(1<<(p-1))][q]+e[q][p]);
                     dp[s][p]=true;
                     g[s][p]=q;
-                    cout<<s<<"->"<<fact(s)<<' '<<q<<"->"<<p<<endl;
+                    cout<<s<<"->"<<fact(s)<<' '<<q<<"->"<<p<<' '<<len[s][p]<<endl;
                 }
             }
         }
@@ -39,31 +47,15 @@ int main(){
     int s=(1<<n)-1;
     stack<int>output;
     for(int i=1;i<=n;i++){
-        cout<<dp[s][i]<<' ';
+        cout<<len[s][i]<<' ';
     }
     cout<<endl;
-    int flag=1;
+    int ans=0x3f3f3f3f;
     for(int i=1;i<=n;i++){
-        if(dp[s][i]&&e[i][1]){
-            int p=i;
-            output.push(p);
-            do{
-                output.push(g[s][p]);
-                int t=p;
-                p=g[s][p];
-                s-=(1<<(t-1));
-            }while(s!=1);
-            flag=0;
-            break;
+        if(dp[s][i]){
+            ans=min(ans,len[s][i]);
         }
     }
-    if(flag=1){
-        cout<<-1<<endl;
-        return 0;
-    }
-    while(!output.empty()){
-        cout<<output.top()<<' ';
-        output.pop();
-    }
+    cout<<ans<<endl;
     return 0;
 }
