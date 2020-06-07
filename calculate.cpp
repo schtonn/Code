af5d38d4bb9990651c9447ad320a33b6d2624a89
schtonn <m18519511495@163.com>
2020-06-07 10:23:19 +0800

    domino v1

--- calculate.cpp
+++ calculate.cpp
@@ -1,96 +1,27 @@
 #include "bits/stdc++.h"
 using namespace std;
-int x,y,ans;
-int dirx[]={0,0,1,-1};
-int diry[]={1,-1,0,0};
-void theFatUglyLargeWizardishDepthFirstSearch(vector<string> chess,int step){
-    if(step>10||step>=ans)return;
-    int ax=-1,bx=-1,ay=-1,by=-1;
-    int axN=-1,bxN=-1,ayN=-1,byN=-1;
-    for(int i=0;i<x;i++){
-        for(int j=0;j<y;j++){
-            if(chess[i][j]=='o'&&ax==-1){
-                ax=i;ay=j;
-                continue;
-            }
-            else if(chess[i][j]=='o'&&ax!=-1){
-                bx=i;by=j;
-                break;
-            }
-        }
-    }
-    for(int i=0;i<4;i++){
-        int tmp_r=ax+dirx[i];
-        int tmp_c=ay+diry[i];
-        bool moved=false;
-        int counter=0;
-        if(tmp_r<0||tmp_r>=x||tmp_c<0||tmp_c>=y){
-            counter++;
-        }
-        tmp_r=bx+dirx[i];
-        tmp_c=by+diry[i];
-        if(tmp_r<0||tmp_r>=x||tmp_c<0||tmp_c>=y){
-            counter++;
-        }
-        if(counter==1){
-            if(step<ans)ans=step;
-            return;
-        }
-        if(counter==2)continue;
-        tmp_r=ax+dirx[i];
-        tmp_c=ay+diry[i];
-        if(tmp_r>=0&&tmp_r<x&&tmp_c>=0&&tmp_c<y&&chess[tmp_r][tmp_c]!='#'){
-            axN=tmp_r;
-            ayN=tmp_c;
-            moved=true;
-        }
-        else{
-            axN=ax;
-            ayN=ay;
-        }
-        tmp_r=bx+dirx[i];
-        tmp_c=by+diry[i];
-        if(tmp_r>=0||tmp_r<x||tmp_c>=0||tmp_c<y){
-            counter++;
-        }
-        if(tmp_r>=0&&tmp_r<x&&tmp_c>=0&&tmp_c<y&&chess[tmp_r][tmp_c]!='#'){
-            bxN=tmp_r;
-            byN=tmp_c;
-            moved=true;
-        }
-        else{
-            bxN=bx;
-            byN=by;
-        }
-        if(axN==bxN&&ayN==byN)continue;
-        if(moved==false)continue;
-        vector<string>chN=chess;
-        chN[ax][ay]='.';
-        chN[bx][by]='.';
-        chN[axN][ayN]='o';
-        chN[bxN][byN]='o';
-        theFatUglyLargeWizardishDepthFirstSearch(chN,step+1);
+const int N=1<<10;
+int n=3,m;
+int F[N][25];//F[i][B]表示骨牌覆盖完前i行，多余的骨牌状态为j的方案总数。
+int fact(int n) {
+    if(n<2)return n;
+    else{
+        return fact(n/2)*10+n%2;
     }
 }
-
-int theExterierOfTheFatUglyLargeWizardishDepthFirstSearch(vector <string> chess) {	
-    x=chess.size();
-    y=chess[0].size();
-    ans=50;
-    theFatUglyLargeWizardishDepthFirstSearch(chess,1);
-    if(ans>10)return -1;
-    return ans;
+bool T(int A,int B){
+    int C=A|B;
+    int D=C^((1<<m)-1);
+    int E=D/3;
+    int F=E|(E<<1);
+    cout<<fact(A)<<",B="<<fact(B)<<",C="<<fact(C)<<",D="<<fact(D)<<",E="<<fact(E)<<",F="<<fact(F);
+    if(A&B)return false;
+    if(F==D)return true;
+    return false;
 }
-vector<string>chess;
 int main(){
-    freopen("CoinsGameEasy.in","r",stdin);
-    freopen("CoinsGameEasy.out","w",stdout);
-    cin>>x>>y;
-    string theSmartTempOfInput;
-    for(int i=0;i<x;i++){
-        cin>>theSmartTempOfInput;
-        chess.push_back(theSmartTempOfInput);
-    }
-    cout<<theExterierOfTheFatUglyLargeWizardishDepthFirstSearch(chess)<<endl;
-    system("pause");
+    int A,B;
+    cin>>m>>A>>B;
+    cout<<T(A,B)<<endl;
+    return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,61 +1,37 @@
 #include "bits/stdc++.h"
 using namespace std;
 const int N=1<<10;
-int n,m,g[N][25];
-int e[25][25];
-int dp[N][25];//dp[i][j]表示状态为i，最后到达j点有没有路径
-int len[N][25];
-int special[N][3];
-int fact(int n) {
-    if(n<2)return n;
+const int mod=10007;
+int m,n;
+int F[100][N];//F[i][j]表示骨牌覆盖完前i行，多余的骨牌状态为j的方案总数。
+int fact(int m) {
+    if(m<2)return m;
     else{
-        return fact(n/2)*10+n%2;
+        return fact(m/2)*10+m%2;
     }
 }
-
+bool T(int A,int B){
+    if(A&B)return false;
+    int C=A|B;
+    int D=C^((1<<n)-1);
+    int E=D/3;
+    // cout<<fact(A)<<','<<fact(B)<<','<<fact(C)<<','<<fact(D)<<','<<fact(E);
+    int F=E|(E<<1);
+    if(F==D)return true;
+    return false;
+}
 int main(){
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            int c;
-            cin>>c;
-            e[i][j]=c;
-        }
-    }
-    for(int i=1;i<=n;i++){
-        dp[1<<(i-1)][i]=1;
-        special[1<<(i-1)][1]=-1;
-        special[1<<(i-1)][2]=i;
-    }
-    for(int s=1;s<(1<<n);++s){
-        for(int p=1;p<=n;++p){
-            if(s&(1<<(p-1))==0)continue;//判断p是否为S中元素
-            for(int q=1;q<=n;++q){
-                if(s&(1<<(q-1))==0)continue;//判断q是否为S中元素
-                if(e[q][p]==-1)continue;//判断有没有q到p的边
-                if(dp[s^(1<<(p-1))][q]){//判断dp[{s}-p][q]是否为真
-                    if(len[s][p]==0)len[s][p]=len[s^(1<<(p-1))][q]+e[q][p];
-                    else len[s][p]=min(len[s][p],len[s^(1<<(p-1))][q]+e[q][p]);
-                    dp[s][p]=true;
-                    g[s][p]=q;
-                    cout<<s<<"->"<<fact(s)<<' '<<q<<"->"<<p<<' '<<len[s][p]<<endl;
-                }
+    cin>>n>>m;
+    F[0][0]=1;
+    for(int i=1;i<=m;i++){//行数
+        for(int cur=0;cur<=(1<<n)-1;cur++){//枚举本行状态(000~111)
+            for(int pre=0;pre<=(1<<n)-1;pre++){//枚举上一行状态
+                int k=T(pre,cur);
+                F[i][cur]=(F[i][cur]+F[i-1][pre]*k)%mod;
+                // cout<<","<<i<<","<<cur<<","<<F[i][cur]<<","<<pre<<","<<fact(i-1)<<","<<F[i-1][pre]<<","<<k<<endl;
             }
         }
     }
-    cout<<endl;
-    int s=(1<<n)-1;
-    stack<int>output;
-    for(int i=1;i<=n;i++){
-        cout<<len[s][i]<<' ';
-    }
-    cout<<endl;
-    int ans=0x3f3f3f3f;
-    for(int i=1;i<=n;i++){
-        if(dp[s][i]){
-            ans=min(ans,len[s][i]);
-        }
-    }
-    cout<<ans<<endl;
+    cout<<F[m][0]<<endl;
     return 0;
 }
