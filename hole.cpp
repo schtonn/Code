9d0299db41eada6b44f723d90b39b1f9326c034e
schtonn <m18519511495@163.com>
2020-06-13 11:46:02 +0800

    domino v3 -2

--- hole.cpp
+++ hole.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 const int N=1<<10;
 const int mod=10007;
-int m,n;
+int m,n,p;
 int F[100][N];//F[i][j]表示骨牌覆盖完前i行，多余的骨牌状态为j的方案总数。
+int hole[100];
 int fact(int m) {
     if(m<2)return m;
     else{
@@ -11,24 +12,33 @@ int fact(int m) {
     }
 }
 bool T(int A,int B){
-    if(A&B)return false;
     int C=A|B;
     int D=C^((1<<n)-1);
     int E=D/3;
-    // cout<<fact(A)<<','<<fact(B)<<','<<fact(C)<<','<<fact(D)<<','<<fact(E);
+    cout<<fact(A)<<','<<fact(B)<<','<<fact(C)<<','<<fact(D)<<','<<fact(E);
     int F=E|(E<<1);
+    if(A&B)return false;
     if(F==D)return true;
     return false;
 }
 int main(){
-    cin>>n>>m;
+    freopen("Code\\f.test","w",stdout);
+    cin>>n>>m>>p;
+    int x,y;
+    for(int i=1;i<=p;i++){
+        cin>>x>>y;
+        hole[y]|=1<<(x-1);
+    }
+    for(int i=1;i<=m;i++){
+        cout<<fact(hole[i])<<endl;
+    }
     F[0][0]=1;
     for(int i=1;i<=m;i++){//行数
-        for(int cur=0;cur<=(1<<n)-1;cur++){//枚举本行状态(000~111)
-            for(int pre=0;pre<=(1<<n)-1;pre++){//枚举上一行状态
-                int k=T(pre,cur);
-                F[i][cur]=(F[i][cur]+F[i-1][pre]*k)%mod;
-                // cout<<","<<i<<","<<cur<<","<<F[i][cur]<<","<<pre<<","<<fact(i-1)<<","<<F[i-1][pre]<<","<<k<<endl;
+        for(int B=0;B<=(1<<n)-1;B++){//枚举本行状态
+            for(int A=0;A<=(1<<n)-1;A++){//枚举上一行状态
+                int k=T(A,B|hole[i]);
+                F[i][B]=(F[i][B]+F[i-1][A]*k)%mod;
+                cout<<","<<i<<","<<F[i][B]<<","<<F[i-1][A]<<","<<k<<endl;
             }
         }
     }
