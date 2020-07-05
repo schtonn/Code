726fed44dd617535b5001095f516bc8b2bd7e23d
schtonn <m18519511495@163.com>
2020-07-05 16:03:19 +0800

    stair v2

--- hamar.cpp
+++ hamar.cpp
@@ -1,14 +1,17 @@
 #include "bits/stdc++.h"
 using namespace std;
-int n,a[100010],dp[100010];
+long long n,a[1000010],dp[1000010];
 int main(){
     cin>>n;
     for(int i=1;i<=n;i++){
         cin>>a[i];
     }
+    dp[0]=0;
     dp[1]=a[1];
-    for(int i=2;i<=n;i++){
-        dp[i]=max(dp[i-1],dp[i-2])+a[i];
+    dp[2]=max(dp[0],dp[1])+a[2];
+    dp[3]=max(max(dp[0],dp[1]),dp[2])+a[3];
+    for(int i=4;i<=n;i++){
+        dp[i]=max(max(max(dp[i-1],dp[i-2]),dp[i-3]),dp[i-4])+a[i];
     }
     cout<<dp[n]<<endl;
     return 0;
