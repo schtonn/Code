4ddf6ec3f46a00cbf8d0870bd2c072ccdf25cea4
schtonn <m18519511495@163.com>
2020-07-07 23:36:30 +0800

    long up sub

--- hamar.cpp
+++ hamar.cpp
@@ -1,18 +1,21 @@
 #include "bits/stdc++.h"
 using namespace std;
-long long n,a[1000010],dp[1000010];
+int n,a[1010],dp[1010];
 int main(){
     cin>>n;
     for(int i=1;i<=n;i++){
         cin>>a[i];
+        dp[i]=1;
     }
-    dp[0]=0;
-    dp[1]=a[1];
-    dp[2]=max(dp[0],dp[1])+a[2];
-    dp[3]=max(max(dp[0],dp[1]),dp[2])+a[3];
-    for(int i=4;i<=n;i++){
-        dp[i]=max(max(max(dp[i-1],dp[i-2]),dp[i-3]),dp[i-4])+a[i];
+    int ans=1;
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<i;j++){
+            if(a[j]<a[i]){
+                dp[i]=max(dp[i],dp[j]+1);
+                ans=max(ans,dp[i]);
+            }
+        }
     }
-    cout<<dp[n]<<endl;
+    cout<<ans<<endl;
     return 0;
 }
