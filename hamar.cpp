0b2e7a116e5bdec3367c29abb27ef07d4f1aac22
schtonn <m18519511495@163.com>
2020-07-28 20:29:36 +0800

    p to 10

--- hamar.cpp
+++ hamar.cpp
@@ -1,14 +1,19 @@
 #include "bits/stdc++.h"
 using namespace std;
-int n,fact,out[100],cnt;
+string fAKe;
+int n,num[100],fact,ans,tool=1;
 int main(){
-    cin>>n>>fact;
-    while(n){
-        out[cnt++]=n%fact;
-        n/=fact;
+    cin>>fact>>fAKe;
+    n=fAKe.length();
+    for(int i=0;i<n;i++){
+        if(fAKe[i]>='0'&&fAKe[i]<='9')num[i]=fAKe[i]-'0';
+        if(fAKe[i]>='A'&&fAKe[i]<='Z')num[i]=fAKe[i]-'A'+10;
     }
-    for(int i=cnt-1;i>=0;i--){
-        cout<<out[i];
+    for(int i=n-1;i>=0;i--){
+        cout<<ans<<endl;
+        ans+=num[i]*tool;
+        tool*=fact;
     }
+    cout<<ans<<endl;
     return 0;
 }
