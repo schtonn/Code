a99c706c3f861cf65af7a6f15576cd4c7df32925
schtonn <m18519511495@163.com>
2020-07-31 21:48:10 +0800

    mul -1

--- hamar.cpp
+++ hamar.cpp
@@ -1,7 +1,8 @@
 #include "bits/stdc++.h"
 using namespace std;
-int n,a[1010],b[1010],c[1010];
-char frog[1010];
+#define MAXN 10010
+int n,a[MAXN],b[MAXN],c[MAXN];
+char frog[MAXN];
 void frogger(int grof[]){//frogger that
     cin>>frog;
     n=strlen(frog);
@@ -11,15 +12,18 @@ int main(){
     frogger(a);
     frogger(b);
     memset(c,0,sizeof(c));
-    for(int i=0;i<1000;i++){
-        c[i]+=a[i]+b[i];
+    for(int i=0;i<MAXN-1;i++){
+        for(int j=0;j<=i;j++){
+            c[i]+=a[j]*b[i-j];
+        }
         if(c[i]>=10){
-            c[i+1]++;
-            c[i]-=10;
+            c[i+1]+=c[i]/10;
+            c[i]%=10;
         }
     }
-    int frocnt=1010;
-    while(!c[frocnt])frocnt--;
+    int frocnt=MAXN;
+    while(!c[frocnt]&&frocnt>=0)frocnt--;
     for(int i=frocnt;i>=0;i--)cout<<c[i];
+    if(frocnt==-1)cout<<0<<endl;
     return 0;
 }
