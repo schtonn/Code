c4348b8d73e54a5963d4e79d54801c0b56980923
schtonn <m18519511495@163.com>
2020-08-07 20:16:36 +0800

    rightnum

--- hamar.cpp
+++ hamar.cpp
@@ -1,15 +1,21 @@
 #include "bits/stdc++.h"
 using namespace std;
-long long a,b,mod,ans=1;
+long long a,b,mod=10,ans=1;
+int n;
 int main(){
     // freopen("mason.in","r",stdin);
     // freopen("mason.out","w",stdout);
-    cin>>a>>b>>mod;
-    while(b){
-        if(b&1)ans=ans*a%mod;
-        a=a*a%mod;
-        b>>=1;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        ans=1;
+        cin>>a;
+        b=a;
+        while(b){
+            if(b&1)ans=ans*a%mod;
+            a=a*a%mod;
+            b>>=1;
+        }
+        cout<<ans<<endl;
     }
-    cout<<ans<<endl;
     return 0;
 }
