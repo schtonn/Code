7d9aeae56b33ad2f14efc57191f313c3ca352437
schtonn <m18519511495@163.com>
2020-08-08 21:46:06 +0800

    SemiPerfectSquare

--- hamar.cpp
+++ hamar.cpp
@@ -1,23 +1,18 @@
 #include "bits/stdc++.h"
 using namespace std;
-const long long mod=100003;
-long long n,m;
-long long qpow(long long a,long long b){
-    long long ans=1;
-    a%=mod;
-    while(b){
-        if(b&1)ans=(ans*a)%mod;
-        a=(a*a)%mod;
-        b>>=1;
-    }
-    return ans;
-}
+int n,ans;
 int main(){
-    // freopen("mason.in","r",stdin);
-    // freopen("mason.out","w",stdout);
-    cin>>m>>n;
-    m%=mod;
-    n%=mod;
-    cout<<((qpow(m,n)%mod-(m*qpow(m-1,n-1)%mod))+mod)%mod;
+    // freopen("SemiPerfectSquare.in","r",stdin);
+    // freopen("SemiPerfectSquare.out","w",stdout);
+    cin>>n;
+    for(int i=2;i<35;i++){
+        for(int j=1;j<i;j++){
+            if(i*i*j==n){
+                ans=1;
+            }
+        }
+    }
+    if(ans)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
     return 0;
 }
