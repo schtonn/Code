03d30751304d072276f83d317c3c029045f20b0d
schtonn <m18519511495@163.com>
2020-08-08 21:40:21 +0800

    break-jail

--- hamar.cpp
+++ hamar.cpp
@@ -1,17 +1,23 @@
 #include "bits/stdc++.h"
 using namespace std;
-long long a,b,mod,ans;
-int main(){
-    // freopen("mason.in","r",stdin);
-    // freopen("mason.out","w",stdout);
-    cin>>a>>b>>mod;
+const long long mod=100003;
+long long n,m;
+long long qpow(long long a,long long b){
+    long long ans=1;
     a%=mod;
-    ans=1%mod;
     while(b){
         if(b&1)ans=(ans*a)%mod;
         a=(a*a)%mod;
         b>>=1;
     }
-    cout<<ans<<endl;
+    return ans;
+}
+int main(){
+    // freopen("mason.in","r",stdin);
+    // freopen("mason.out","w",stdout);
+    cin>>m>>n;
+    m%=mod;
+    n%=mod;
+    cout<<((qpow(m,n)%mod-(m*qpow(m-1,n-1)%mod))+mod)%mod;
     return 0;
 }
