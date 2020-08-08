2267c0e16e5f9ced281b53b566d06afac57e2337
schtonn <m18519511495@163.com>
2020-08-08 21:14:00 +0800

    qpow

--- hamar.cpp
+++ hamar.cpp
@@ -1,21 +1,17 @@
 #include "bits/stdc++.h"
 using namespace std;
-long long a,b,mod=10,ans=1;
-int n;
+long long a,b,mod,ans;
 int main(){
     // freopen("mason.in","r",stdin);
     // freopen("mason.out","w",stdout);
-    cin>>n;
-    for(int i=0;i<n;i++){
-        ans=1;
-        cin>>a;
-        b=a;
-        while(b){
-            if(b&1)ans=ans*a%mod;
-            a=a*a%mod;
-            b>>=1;
-        }
-        cout<<ans<<endl;
+    cin>>a>>b>>mod;
+    a%=mod;
+    ans=1%mod;
+    while(b){
+        if(b&1)ans=(ans*a)%mod;
+        a=(a*a)%mod;
+        b>>=1;
     }
+    cout<<ans<<endl;
     return 0;
 }
