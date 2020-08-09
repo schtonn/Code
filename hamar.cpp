0cf1004d61e44840277474061ac7afc9d5747ef4
schtonn <m18519511495@163.com>
2020-08-09 21:08:36 +0800

    excavations2

--- hamar.cpp
+++ hamar.cpp
@@ -1,87 +1,53 @@
 #include "bits/stdc++.h"
 using namespace std;
-#define MAXN 1010
-struct node{
-    int v,next;
-}e[MAXN];
-int h[MAXN],tot,n,d,con,s,st[MAXN];
-bool vis[MAXN];
-char frog;
-void adde(int u,int v){
-    tot++;
-    e[tot].v=v;
-    e[tot].next=h[u];
-    h[u]=tot;
-}
-queue<int>q;
-void dfs(int u){
-    con++;
-    vis[u]=1;
-    for(int i=h[u];i;i=e[i].next){
-        int v=e[i].v;
-        if(!vis[v])dfs(v);
-    }
-}
-void bfs(int k){
-    while(!q.empty())q.pop();
-    q.push(k);
-    vis[k]=1;
-    while(!q.empty()){
-        int u=q.front();
-        q.pop();
-        s=max(s,st[u]);
-        for(int i=h[u];i;i=e[i].next){
-            int v=e[i].v;
-            if(!vis[v]){
-                vis[v]=1;
-                q.push(v);
-                st[v]=st[u]+1;
-            }
-        }
+#define MAXN 60
+typedef long long ll;
+ll n,C[MAXN][MAXN],a[MAXN],found[MAXN],pos[MAXN],kk,f,kind[MAXN],fos[MAXN],frog;
+void copy(){
+    for(ll i=0;i<=n;i++){
+        fos[i]=pos[i];
     }
 }
 int main(){
-    // freopen("Egalitarianism.in","r",stdin);
-    // freopen("Egalitarianism.out","w",stdout);
+    freopen("Excavations2.in","r",stdin);
+    freopen("Excavations2.out","w",stdout);
     cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>frog;
-            if(frog=='Y'){
-                adde(i,j);
+    for(ll i=0;i<n;i++){
+        cin>>kind[i];
+    }
+    cin>>kk>>f;
+    for(ll i=0;i<f;i++){
+        cin>>found[i];
+    }
+    for(ll i=0;i<f;i++){
+        for(ll j=0;j<n;j++){
+            if(found[i]==kind[j]){
+                a[i]++;
             }
         }
     }
-    cin>>d;
-    dfs(0);
-    if(con<n){
-        cout<<-1<<endl;
-        return 0;
+    C[0][0]=1;
+    for(ll i=1;i<MAXN;i++){
+        C[i][0]=C[i][i]=1;
+        for(ll j=1;j<=i;j++){
+            C[i][j]=C[i-1][j-1]+C[i-1][j];
+        }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++)vis[j]=0;
-        for(int j=0;j<n;j++)st[j]=0;
-        bfs(i);
+    for(ll i=0;i<=a[0];i++){
+        pos[i]=C[a[0]][i];
     }
-    cout<<s*d<<endl;
+    copy();
+    for(ll i=1;i<f;i++){
+        memset(pos,0,sizeof(pos));
+        for(ll j=1;j<MAXN;j++){
+            for(ll k=1;k<MAXN;k++){
+                if(j+k<=kk){
+                    pos[j+k]+=fos[j]*C[a[i]][k];
+                }
+            }
+        }
+        copy();
+    }
+    cout<<pos[kk]<<endl;
     return 0;
 }
-/*
-15
-NYYNNNNYYYYNNNN
-YNNNYNNNNNNYYNN
-YNNYNYNNNNYNNNN
-NNYNNYNNNNNNNNN
-NYNNNNYNNYNNNNN
-NNYYNNYNNYNNNYN
-NNNNYYNNYNNNNNN
-YNNNNNNNNNYNNNN
-YNNNNNYNNNNNYNN
-YNNNYYNNNNNNNNY
-YNYNNNNYNNNNNNN
-NYNNNNNNNNNNNNY
-NYNNNNNNYNNNNYN
-NNNNNYNNNNNNYNN
-NNNNNNNNNYNYNNN
-1
-*/
