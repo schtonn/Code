5b6a12fa4c9249e1217c49278dadf5e71b16a523
schtonn <m18519511495@163.com>
2020-08-12 21:10:02 +0800

    LIS-div2

--- hamar.cpp
+++ hamar.cpp
@@ -1,17 +1,17 @@
 #include "bits/stdc++.h"
 using namespace std;
-const int mod=1000000007;
-int n,ans[1000010],dp[1000010];
+int n,ans,a[100];
 int main(){
-    // freopen("TrafficCongestion.in","r",stdin);
-    // freopen("TrafficCongestion.out","w",stdout);
+    // freopen("LISNumberDivTwo.in","r",stdin);
+    // freopen("LISNumberDivTwo.out","w",stdout);
     cin>>n;
-    ans[0]=ans[1]=dp[0]=1;
-    dp[1]=2;
-    for(int i=2;i<=n;i++){
-        ans[i]=(2*dp[i-2]+1)%mod;
-        dp[i]=(dp[i-1]+ans[i])%mod;
+    ans=n;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        if(i>0&&a[i]>a[i-1]){
+            ans--;
+        }
     }
-    cout<<ans[n]<<endl;
+    cout<<ans<<endl;
     return 0;
 }
