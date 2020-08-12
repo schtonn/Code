b836adc4dd577891b432ae82a674624b8c585a58
schtonn <m18519511495@163.com>
2020-08-12 12:08:48 +0800

    fibonacci

--- hamar.cpp
+++ hamar.cpp
@@ -1,53 +1,67 @@
 #include "bits/stdc++.h"
 using namespace std;
-#define MAXN 60
-typedef long long ll;
-ll n,C[MAXN][MAXN],a[MAXN],found[MAXN],pos[MAXN],kk,f,kind[MAXN],fos[MAXN],frog;
-void copy(){
-    for(ll i=0;i<=n;i++){
-        fos[i]=pos[i];
+const long long N=90;
+const long long mod=1e9+7;
+long long m,n;
+struct matrix{
+    long long v[N+2][N+2];
+    long long x,y;
+}t,I,ans;
+long long pre[N+2];
+ostream&operator<<(ostream&ous,matrix a){  
+    for(long long i=0;i<a.x;i++){
+        for(long long j=0;j<a.y;j++){
+            ous<<a.v[i][j]<<' ';
+        }
+        ous<<endl;
     }
+    return ous;
+}
+matrix operator+(matrix a,matrix b){
+    matrix c;
+    if(a.x!=b.x||a.y!=b.y)return c;
+    long long x=a.x,y=a.y;
+    c.x=x;
+    c.y=y;
+    for(long long i=0;i<x;i++){
+        for(long long j=0;j<y;j++){
+            c.v[i][j]=(a.v[i][j]+b.v[i][j])%mod;
+        }
+    }
+    return c;
+}
+matrix operator*(matrix a,matrix b){
+    matrix c;
+    if(a.y!=b.x)return c;
+    long long x=a.x,y=b.y,z=a.y;
+    c.x=x;
+    c.y=y;
+    for(long long i=0;i<x;i++){
+        for(long long j=0;j<y;j++){
+            c.v[i][j]=0;
+            for(long long k=0;k<z;k++){
+                c.v[i][j]=(c.v[i][j]+a.v[i][k]*b.v[k][j])%mod;
+            }
+        }
+    }
+    return c;
+}
+void init(){
+    t.x=t.y=2;
+    I.x=I.y=2;
+    t.v[0][0]=t.v[0][1]=t.v[1][0]=1;
+    I.v[0][0]=I.v[1][1]=1;
 }
 int main(){
-    freopen("Excavations2.in","r",stdin);
-    freopen("Excavations2.out","w",stdout);
-    cin>>n;
-    for(ll i=0;i<n;i++){
-        cin>>kind[i];
+    cin>>m;
+    init();
+    long long cnt=m-1;
+    ans=I;
+    while(cnt){
+        if(cnt&1)ans=ans*t;
+        t=t*t;
+        cnt>>=1;
     }
-    cin>>kk>>f;
-    for(ll i=0;i<f;i++){
-        cin>>found[i];
-    }
-    for(ll i=0;i<f;i++){
-        for(ll j=0;j<n;j++){
-            if(found[i]==kind[j]){
-                a[i]++;
-            }
-        }
-    }
-    C[0][0]=1;
-    for(ll i=1;i<MAXN;i++){
-        C[i][0]=C[i][i]=1;
-        for(ll j=1;j<=i;j++){
-            C[i][j]=C[i-1][j-1]+C[i-1][j];
-        }
-    }
-    for(ll i=0;i<=a[0];i++){
-        pos[i]=C[a[0]][i];
-    }
-    copy();
-    for(ll i=1;i<f;i++){
-        memset(pos,0,sizeof(pos));
-        for(ll j=1;j<MAXN;j++){
-            for(ll k=1;k<MAXN;k++){
-                if(j+k<=kk){
-                    pos[j+k]+=fos[j]*C[a[i]][k];
-                }
-            }
-        }
-        copy();
-    }
-    cout<<pos[kk]<<endl;
+    cout<<ans.v[0][0]<<endl;
     return 0;
 }
