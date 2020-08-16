a092633bcf60eccc60cd8fa575283f3a674c9b0a
schtonn <m18519511495@163.com>
2020-08-16 16:40:42 +0800

    PiecewiseLinearFunction

--- hamar.cpp
+++ hamar.cpp
@@ -1,126 +1,26 @@
 #include "bits/stdc++.h"
 using namespace std;
-const long long N=90;
-const long long mod=123456789;
-long long n,tlen,initial[N+2];
-string str;
-struct matrix{
-    long long v[N+2][N+2];
-    long long x,y;
-    void clear(){
-        for(long long i=0;i<N;i++){
-            for(long long j=0;j<N;j++){
-                v[i][j]=0;
-            }
-        }
-    }
-}t,I,ans,f;
-long long base[N+2][N+2];
-ostream&operator<<(ostream&ous,matrix a){  
-    for(long long i=0;i<a.x;i++){
-        for(long long j=0;j<a.y;j++){
-            ous<<a.v[j][i]%mod<<' ';
-        }
-        ous<<endl;
-    }
-    return ous;
-}
-matrix operator+(matrix a,matrix b){
-    matrix c;
-    c.clear();
-    if(a.x!=b.x||a.y!=b.y){
-        cout<<"error adding!"<<endl;
-        return c;
-    }
-    long long x=a.x,y=a.y;
-    c.x=x;
-    c.y=y;
-    for(long long i=0;i<x;i++){
-        for(long long j=0;j<y;j++){
-            c.v[i][j]=(a.v[i][j]+b.v[i][j])%mod;
-        }
-    }
-    return c;
-}
-matrix operator*(matrix a,matrix b){
-    matrix c;
-    c.clear();
-    if(a.y!=b.x){
-        cout<<"error multiplying!"<<endl;
-        return c;
-    }
-    long long x=a.x,y=b.y,z=a.y;
-    c.x=x;
-    c.y=y;
-    for(long long i=0;i<x;i++){
-        for(long long j=0;j<y;j++){
-            c.v[i][j]=0;
-            for(long long k=0;k<z;k++){
-                c.v[i][j]=(c.v[i][j]+a.v[i][k]*b.v[k][j])%mod;
-            }
-        }
-    }
-    return c;
-}
-void init(){
-    t.x=t.y=tlen;
-    I.x=I.y=tlen;
-    f.x=tlen;
-    f.y=1;
-    for(long long i=0;i<tlen;i++){
-        for(long long j=0;j<tlen;j++){
-            t.v[i][j]=base[i][j];
-        }
-    }
-    for(long long i=0;i<tlen;i++){
-        f.v[i][0]=initial[i];
-    }
-    for(long long i=0;i<tlen;i++)I.v[i][i]=1;
-}
-long long max_match(string suf){
-    long long pre=0,slen=suf.length(),flag;
-    for(long long i=0;i<slen;i++){
-        flag=1;
-        for(long long j=0;j<slen-i;j++){
-            if(suf[i+j]!=str[j])flag=0;
-        }
-        if(flag){
-            pre=slen-i;
-            break;
-        }
-    }
-    return pre;
-}
-long long qpow(long long a,long long b){
-    long long ans=1;
-    while(b){
-        if(b&1)ans=(ans*a)%mod;
-        a=(a*a)%mod;
-        b>>=1;
-    }
-    return ans;
-}
+int n,m,k,ans,f[110];
 int main(){
-    cin>>str>>n;
-    tlen=str.length();
-    for(long long i=0;i<tlen;i++){
-        base[i][max_match(str.substr(0,i)+'A')]++;
-        base[i][max_match(str.substr(0,i)+'T')]++;
-        base[i][max_match(str.substr(0,i)+'C')]++;
-        base[i][max_match(str.substr(0,i)+'G')]++;
+    // freopen("PiecewiseLinearFunctionDiv2.in","r",stdin);
+    // freopen("PiecewiseLinearFunctionDiv2.out","w",stdout);
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>f[i];
     }
-    init();
-    matrix ans=I;
-    long long cnt=n;
-    while(cnt){
-        if(cnt&1)ans=ans*t;
-        t=t*t;
-        cnt>>=1;
+    cin>>m;
+    for(int i=0;i<m;i++){
+        cin>>k;
+        ans=0;
+        for(int j=0;j<n-1;j++){
+            if(f[j]==k||(f[j]<k&&f[j+1]>k)||(f[j]>k&&f[j+1]<k))ans++;
+            if(f[j]==k&&f[j+1]==k){
+                ans=-1;
+                break;
+            }
+        }
+        if(f[n-1]==k&&ans!=-1)ans++;
+        cout<<ans<<' ';
     }
-    long long wop=qpow(4,n);
-    for(long long i=0;i<tlen;i++){
-        wop=(wop-ans.v[0][i])+mod%mod;
-    }
-    cout<<wop+mod%mod<<endl;
     return 0;
 }
