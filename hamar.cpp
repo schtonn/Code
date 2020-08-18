e0fd703c98fd1a1c75f7b172b8edf62a1f508b3c
schtonn <m18519511495@163.com>
2020-08-18 20:48:04 +0800

    stringweight2

--- hamar.cpp
+++ hamar.cpp
@@ -1,26 +1,33 @@
 #include "bits/stdc++.h"
 using namespace std;
-string a,b,c;
-int f[1010][1010],step[1010][1010],al,bl,cl;
+#define MAXN 1000
+const long long mod=1e9+9;
+long long C[MAXN+10][MAXN+10],l;
+long long fact(long long n){
+    long long ans=1;
+    for(long long i=26;i>26-n;i--){
+        ans=(ans*i)%mod;
+    }
+    return ans%mod;
+}
+long long P(long long n,long long m){
+    long long ans=1;
+    for(long long i=0;i<m;i++){
+        ans=(ans*n)%mod;
+        n--;
+    }
+    return ans;
+}
 int main(){
-    cin>>a>>b;
-    al=a.length();
-    bl=b.length();
-    for(int i=1;i<=al;i++){
-        f[i][0]=i;
-    }
-    for(int i=1;i<=bl;i++){
-        f[0][i]=i;
-    }
-    for(int i=1;i<=al;i++){
-        for(int j=1;j<=bl;j++){
-            int A=f[i-1][j],B=f[i][j-1],C=f[i-1][j-1];
-            f[i][j]=min(min(A,B),C)+1;
-            if(a[i-1]==b[j-1]){
-                f[i][j]=C;
-            }
-        }
-    }
-    cout<<f[al][bl]<<endl;
+    // C[0][0]=1;
+    // for(long long i=1;i<MAXN;i++){
+    //     C[i][0]=C[i][i]=1;
+    //     for(long long j=1;j<=i;j++){
+    //         C[i][j]=(C[i-1][j-1]+C[i-1][j])%mod;
+    //     }
+    // }
+    cin>>l;
+    if(l<=26)cout<<P(26,l)<<endl;
+    else cout<<(P(l-1,25)*26)%mod<<endl;
     return 0;
 }
