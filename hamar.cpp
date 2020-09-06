c499ae9699a9ae5c8a19aa7ead9a1a0386a9d77d
schtonn <m18519511495@163.com>
2020-09-06 15:26:27 +0800

    max-independent-subgroup -2 simple

--- hamar.cpp
+++ hamar.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #else
     #define N 1010
 #endif
-int n,c[N],f[N],dp[N][2],g[N][2],sum0,sum1,sgum[N],ggum;
+int n,c[N],f[N],dp[N][2],sum0,sum1,sgum[N],ggum;
 //dp[u][0]:u为根不选u
 //dp[u][1]:u为根选u
 vector<int>son[N];
@@ -35,16 +35,11 @@ int main(){
         sum0=sum1=0;
         for(int i=0;i<son[u].size();i++){
             sum0+=max(dp[son[u][i]][0],dp[son[u][i]][1]);
-            if(dp[son[u][i]][0]>dp[son[u][i]][1])g[u][0]+=g[son[u][i]][0];
-            else g[u][0]+=g[son[u][i]][1];
             sum1+=dp[son[u][i]][0];
-            g[u][1]+=g[son[u][i]][0];
         }
         dp[u][0]=sum0;
         dp[u][1]=sum1+1;
-        g[u][1]+=c[u];
     }
-    if(dp[1][1]>dp[1][0])cout<<g[1][1]<<endl;
-    else cout<<g[1][0]<<endl;
+    cout<<max(dp[1][0],dp[1][1]);
     return 0;
 }
