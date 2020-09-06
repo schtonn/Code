bcb164e833a978a575c9cee2e6e9f3b0036708d6
schtonn <m18519511495@163.com>
2020-09-06 20:59:04 +0800

    max-independent-subgroup -2 output 80pts

--- hamar.cpp
+++ hamar.cpp
@@ -6,20 +6,23 @@ using namespace std;
 #else
     #define N 1010
 #endif
-int n,c[N],f[N],dp[N][2],sum0,sum1,sgum[N],ggum;
+int n,c[N],f[N],dp[N][2],ka[N],kb[N],sum0,sum1,sgum[N],ggum;
 //dp[u][0]:u为根不选u
 //dp[u][1]:u为根选u
 vector<int>son[N];
+vector<int>ansa[N];
+vector<int>ansb[N];
 vector<int>order;
+vector<int>Null;
 queue<int>q;
+bool cmd(int a,int b){
+    return a<b;
+}
 int main(){
     cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>c[i];
-    }
-    for(int i=1;i<=n;i++){
+    for(int i=2;i<=n;i++){
         cin>>f[i];
-        if(i!=1)son[f[i]].push_back(i);
+        son[f[i]].push_back(i);
     }
     q.push(1);
     while(!q.empty()){
@@ -30,16 +33,47 @@ int main(){
             q.push(son[v][i]);
         }
     }
-    for(int i=n-1;i>=0;i--){
-        int u=order[i];
+    for(int t=n-1;t>=0;t--){
+        int u=order[t];
         sum0=sum1=0;
         for(int i=0;i<son[u].size();i++){
             sum0+=max(dp[son[u][i]][0],dp[son[u][i]][1]);
+            if(dp[son[u][i]][0]>dp[son[u][i]][1]){
+                for(int j=0;j<ansa[son[u][i]].size();j++){
+                    ansa[u].push_back(ansa[son[u][i]][j]);
+                }
+            }else{
+                for(int j=0;j<ansb[son[u][i]].size();j++){
+                    ansa[u].push_back(ansb[son[u][i]][j]);
+                }
+            }
             sum1+=dp[son[u][i]][0];
+            for(int j=0;j<ansa[son[u][i]].size();j++){
+                ansb[u].push_back(ansa[son[u][i]][j]);
+            }
+            Null.swap(ansa[son[u][i]]);
+            Null.swap(ansb[son[u][i]]);
         }
+        ansb[u].push_back(u);
         dp[u][0]=sum0;
         dp[u][1]=sum1+1;
     }
-    cout<<max(dp[1][0],dp[1][1]);
+    if(dp[1][0]>dp[1][1]){
+        for(int i=0;i<ansa[1].size();i++){
+            ka[i]=ansa[1][i];
+        }
+        sort(ka,ka+ansa[1].size());
+        for(int i=0;i<ansa[1].size();i++){
+            cout<<ka[i]<<' ';
+        }
+    }else{
+        for(int i=0;i<ansb[1].size();i++){
+            kb[i]=ansb[1][i];
+        }
+        sort(kb,kb+ansb[1].size());
+        for(int i=0;i<ansb[1].size();i++){
+            cout<<kb[i]<<' ';
+        }
+    }
     return 0;
 }
