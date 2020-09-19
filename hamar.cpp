dead2bbf77a9e945b3e4c2a761b6bc2e074e3eca
schtonn <m18519511495@163.com>
2020-09-19 18:52:49 +0800

    bus

--- hamar.cpp
+++ hamar.cpp
@@ -1,79 +1,40 @@
 #include "bits/stdc++.h"
 using namespace std;
-#define DEBUG
-#ifndef DEBUG
-    #define N 500010
-#else
-    #define N 1010
-#endif
-int n,c[N],f[N],dp[N][2],ka[N],kb[N],sum0,sum1,sgum[N],ggum;
-//dp[u][0]:u为根不选u
-//dp[u][1]:u为根选u
-vector<int>son[N];
-vector<int>ansa[N];
-vector<int>ansb[N];
-vector<int>order;
-vector<int>Null;
-queue<int>q;
-bool cmd(int a,int b){
-    return a<b;
-}
+int n,way,price,t,ans;
+struct ticket{
+    int p,t;
+};
+queue<ticket>q;
 int main(){
     cin>>n;
-    for(int i=2;i<=n;i++){
-        cin>>f[i];
-        son[f[i]].push_back(i);
-    }
-    q.push(1);
-    while(!q.empty()){
-        int v=q.front();
-        q.pop();
-        order.push_back(v);
-        for(int i=0;i<son[v].size();i++){
-            q.push(son[v][i]);
-        }
-    }
-    for(int t=n-1;t>=0;t--){
-        int u=order[t];
-        sum0=sum1=0;
-        for(int i=0;i<son[u].size();i++){
-            sum0+=max(dp[son[u][i]][0],dp[son[u][i]][1]);
-            if(dp[son[u][i]][0]>dp[son[u][i]][1]){
-                for(int j=0;j<ansa[son[u][i]].size();j++){
-                    ansa[u].push_back(ansa[son[u][i]][j]);
+    for(int i=0;i<n;i++){
+        cin>>way>>price>>t;
+        while(t-q.front().t>45&&!q.empty())q.pop();
+        if(way){
+            if(!q.empty()){
+                int ql=q.size();
+                bool usd=0;
+                for(int j=0;j<ql;j++){
+                    if(!usd&&q.front().p>=price){
+                        q.pop();
+                        usd=1;
+                    }else{
+                        q.push(q.front());
+                        q.pop();
+                    }
                 }
+                if(usd==0)ans+=price;
             }else{
-                for(int j=0;j<ansb[son[u][i]].size();j++){
-                    ansa[u].push_back(ansb[son[u][i]][j]);
-                }
+                ans+=price;
             }
-            sum1+=dp[son[u][i]][0];
-            for(int j=0;j<ansa[son[u][i]].size();j++){
-                ansb[u].push_back(ansa[son[u][i]][j]);
-            }
-            Null.swap(ansa[son[u][i]]);
-            Null.swap(ansb[son[u][i]]);
-        }
-        ansb[u].push_back(u);
-        dp[u][0]=sum0;
-        dp[u][1]=sum1+1;
-    }
-    if(dp[1][0]>dp[1][1]){
-        for(int i=0;i<ansa[1].size();i++){
-            ka[i]=ansa[1][i];
-        }
-        sort(ka,ka+ansa[1].size());
-        for(int i=0;i<ansa[1].size();i++){
-            cout<<ka[i]<<' ';
-        }
-    }else{
-        for(int i=0;i<ansb[1].size();i++){
-            kb[i]=ansb[1][i];
-        }
-        sort(kb,kb+ansb[1].size());
-        for(int i=0;i<ansb[1].size();i++){
-            cout<<kb[i]<<' ';
+        }else{
+            ticket a;
+            a.t=t;
+            a.p=price;
+            q.push(a);
+            ans+=price;
         }
     }
+    cout<<ans<<endl;
     return 0;
 }
