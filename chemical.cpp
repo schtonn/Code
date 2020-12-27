90bbe1f654371642f57bca315264447d915a7384
schtonn <m18519511495@163.com>
2020-12-27 22:38:38 +0800

    simplify

--- chemical.cpp
+++ chemical.cpp
@@ -193,19 +193,28 @@ void clean(){
         ans[i]=1;
     }
 }
+void declare(bool t){
+    if(t){
+        cout<<"4Au+8NaCN+2H2O+O2=4Na(Au(CN)2)+4NaOH"<<endl<<endl;
+    }else{
+        cout<<"\t<equation> ::= <expr> \"=\" <expr>"<<endl;
+        cout<<"\t<expr> ::= <coef> <formula> | <expr> \"+\" <coef> <formula>"<<endl;
+        cout<<"\t<coef> ::= <digits> | \"\""<<endl;
+        cout<<"\t<digits> ::= <digit> | <digits> <digit>"<<endl;
+        cout<<"\t<digit> ::= \"0\" | \"1\" | ... | \"9\""<<endl;
+        cout<<"\t<formula> ::= <term> <coef> | <formula> <term> <coef>"<<endl;
+        cout<<"\t<term> ::= <element> | \"(\" <formula> \")\""<<endl;
+        cout<<"\t<element> ::= <uppercase> | <uppercase> <lowercase>"<<endl;
+        cout<<"\t<uppercase> ::= \"A\" | \"B\" | ... | \"Z\""<<endl;
+        cout<<"\t<lowercase> ::= \"a\" | \"b\" | ... | \"z\""<<endl<<endl;
+    }
+}
 void help(){
     cout<<"INPUT"<<endl;
     cout<<"The program requires an chemical equation(<equation>) and outputs wether it is balanced. An equation should be settled as below, or the program may not work properly."<<endl;
-    cout<<"\t<equation> ::= <expr> \"=\" <expr>"<<endl;
-    cout<<"\t<expr> ::= <coef> <formula> | <expr> \"+\" <coef> <formula>"<<endl;
-    cout<<"\t<coef> ::= <digits> | \"\""<<endl;
-    cout<<"\t<digits> ::= <digit> | <digits> <digit>"<<endl;
-    cout<<"\t<digit> ::= \"0\" | \"1\" | ... | \"9\""<<endl;
-    cout<<"\t<formula> ::= <term> <coef> | <formula> <term> <coef>"<<endl;
-    cout<<"\t<term> ::= <element> | \"(\" <formula> \")\""<<endl;
-    cout<<"\t<element> ::= <uppercase> | <uppercase> <lowercase>"<<endl;
-    cout<<"\t<uppercase> ::= \"A\" | \"B\" | ... | \"Z\""<<endl;
-    cout<<"\t<lowercase> ::= \"a\" | \"b\" | ... | \"z\""<<endl<<endl;
+    declare(0);
+    cout<<"For example: ";
+    declare(1);
     cout<<"OUTPUT"<<endl;
     cout<<"\"Y\" means this equation is balanced."<<endl;
     cout<<"\"N\" means this equation is unbalanced."<<endl;
@@ -216,24 +225,17 @@ void help(){
     cout<<"Warning: This program supports up to 1000 expressions with 1000 elements, but no more."<<endl<<endl;
     cout<<"输入"<<endl;
     cout<<"程序输入一个化学式，并判断其是否配平。化学式的格式如下，不合法的化学式可能引起未定义行为。"<<endl;
-    cout<<"\t<equation> ::= <expr> \"=\" <expr>"<<endl;
-    cout<<"\t<expr> ::= <coef> <formula> | <expr> \"+\" <coef> <formula>"<<endl;
-    cout<<"\t<coef> ::= <digits> | \"\""<<endl;
-    cout<<"\t<digits> ::= <digit> | <digits> <digit>"<<endl;
-    cout<<"\t<digit> ::= \"0\" | \"1\" | ... | \"9\""<<endl;
-    cout<<"\t<formula> ::= <term> <coef> | <formula> <term> <coef>"<<endl;
-    cout<<"\t<term> ::= <element> | \"(\" <formula> \")\""<<endl;
-    cout<<"\t<element> ::= <uppercase> | <uppercase> <lowercase>"<<endl;
-    cout<<"\t<uppercase> ::= \"A\" | \"B\" | ... | \"Z\""<<endl;
-    cout<<"\t<lowercase> ::= \"a\" | \"b\" | ... | \"z\""<<endl<<endl;
+    declare(0);
+    cout<<"例如: ";
+    declare(1);
     cout<<"输出"<<endl;
-    cout<<"\"Y\" 代表化学式已配平"<<endl;
-    cout<<"\"N\" 代表化学式未配平"<<endl;
-    cout<<"\"unknown elemet\" 代表化学式中的某一元素还没有被发现，或者输入的化学式不合法"<<endl<<endl;
+    cout<<"\"Y\" 代表化学式已配平。"<<endl;
+    cout<<"\"N\" 代表化学式未配平。"<<endl;
+    cout<<"\"unknown elemet\" 代表化学式中的某一元素还没有被发现，或者输入的化学式不合法。"<<endl<<endl;
     cout<<"指令"<<endl;
-    cout<<"help\t提供帮助信息"<<endl;
-    cout<<"exit\t退出程序"<<endl<<endl;
-    cout<<"警告：此程序最多接受1000种元素，和1000个分子式，否则同样会引发未定义行为。"<<endl;
+    cout<<"help\t提供帮助信息。"<<endl;
+    cout<<"exit\t退出程序。"<<endl<<endl;
+    cout<<"警告：此程序最多接受1000种元素，和1000个分子式，否则可能会引发未定义行为。"<<endl;
 }
 void print(){
     for(int i=0;i<rf;i++){
@@ -291,7 +293,9 @@ int main(){
             if(flag){
                 cout<<"N"<<endl;
                 balance();
+                cout<<"Attempt to balance: ";
                 print();
+                cout<<"Beware, the attempt will come out unsuccessful most of the times."<<endl;
             }
             else cout<<"Y"<<endl;
         }
