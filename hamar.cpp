1d56befc3fb38e5b6ae87d234037d923f7a12d3a
schtonn <m18519511495@163.com>
2021-01-08 22:18:17 +0800

    P1419

--- hamar.cpp
+++ hamar.cpp
@@ -1,25 +1,38 @@
-#include<iostream>
-#include<cstring>
-#define MAXN 1000010
+//https://www.luogu.com.cn/record/44690094
+#include "bits/stdc++.h"
 using namespace std;
-int nxt[MAXN];
-int la,lb,k;
-char txt[MAXN],pat[MAXN];
+typedef long long ll;
+#define N 100010
+const double eps=1e-5;
+ll n,S,T,a[N],q[N];
+double t[N];
+bool check(double mid){
+    t[0]=0;
+    for(int i=1;i<=n;i++){
+        t[i]=t[i-1]+a[i]-mid;
+    }
+    ll h=0,b=1;
+    for(int i=1;i<=n;i++){
+        if(i>=S){
+            while(h>=b&&t[i-S]<t[q[h]])h--;
+            q[++h]=i-S;
+        }
+        if(b<=h){
+            if(q[b]<i-T)b++;
+            if(t[i]-t[q[b]]>=0)return true;
+        }
+    }
+    return false;
+}
 int main(){
-    cin>>pat+1;
-    cin>>txt+1;
-    la=strlen(txt+1);
-    lb=strlen(pat+1);
-    for(int i=2;i<=lb;i++){
-        while(k&&pat[i]!=pat[k+1])k=nxt[k];
-        if(pat[k+1]==pat[i])k++;
-        nxt[i]=k;
+    cin>>n>>S>>T;
+    for(int i=1;i<=n;i++)cin>>a[i];
+    double l=-10001,r=10001;
+    while(r-l>eps){
+        double mid=(l+r)/2;
+        if(check(mid))l=mid;
+        else r=mid;
     }
-    k=0;
-    for(int i=1;i<=la;i++){
-        while(k>0&&pat[k+1]!=txt[i])k=nxt[k];
-        if(pat[k+1]==txt[i])k++;
-        if(k==lb){cout<<i-lb+1<<' ';k=nxt[k];}
-    }
+    printf("%.3lf",l);
     return 0;
 }
