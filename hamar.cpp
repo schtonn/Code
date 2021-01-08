1e2d66c21821fca950bea3844a1039fb0800e331
schtonn <m18519511495@163.com>
2021-01-08 22:18:40 +0800

    P3500

--- hamar.cpp
+++ hamar.cpp
@@ -1,39 +1,30 @@
-//https://www.luogu.com.cn/record/44717629
+//https://www.luogu.com.cn/record/44718759
 #include "bits/stdc++.h"
 using namespace std;
-typedef long long ll;
-typedef double dou;
-#define N 100010
-const dou eps=1e-5;
-ll n,S,T,a[N];
-dou post[N],pre[N];
-dou t[N];
-dou max(dou a,dou b){return a>b?a:b;}
-dou min(dou a,dou b){return a<b?a:b;}
-bool check(dou mid){
-    t[0]=0;
-    for(int i=1;i<=n;i++){
-        t[i]=t[i-1]+a[i]-mid;
-    }
-    for(int i=n-1;i>=2;i--)post[i]=max(post[i+1],t[i]);
-    for(int i=2;i<=n-1;i++)pre[i]=min(pre[i-1],t[i]);
-    for(int i=2;i<=n-1;i++){
-        if(post[i]-pre[i-1]>t[n])return false;
-    }
-    return true;
-}
+const int N=1000010;
+int n,m,pat[N],txt[N];
+vector<int>pos[N];
 int main(){
     cin>>n;
-    post[n]=-INT_MAX;
-    pre[1]=INT_MAX;
-    for(int i=1;i<=n;i++)cin>>a[i];
-    dou l=0,r=(a[1]+a[n])/2;
-    while(r-l>eps){
-        dou mid=(l+r)/2;
-        if(check(mid))l=mid;
-        else r=mid;
+    for(int i=1;i<=n;i++){
+        cin>>pat[i];
+        pos[pat[i]].push_back(i);
     }
-    printf("%.3lf",l);
+    cin>>m;
+    for(int i=1;i<=m;i++){
+        int L,p=0,flag=0;
+        cin>>L;
+        for(int j=1;j<=L;j++)cin>>txt[j];
+        for(int j=1;j<=L;j++){
+            vector<int>::iterator t=upper_bound(pos[txt[j]].begin(),pos[txt[j]].end(),p);
+            if(pos[txt[j]].end()==t){
+                flag=1;
+                cout<<"NIE"<<endl;
+                break;
+            }else p=*t;
+        }
+        if(!flag)cout<<"TAK"<<endl;
+    }
     return 0;
 }
 /*
