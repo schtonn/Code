0bad227289603cc710c9074e438ed9f7ff3f39f1
schtonn <m18519511495@163.com>
2021-02-15 18:21:10 +0800

    make Dinic better

--- Algorithms/Dinic.cpp
+++ Algorithms/Dinic.cpp
@@ -1,89 +1,67 @@
 #include "bits/stdc++.h"
 using namespace std;
- 
-const int Maxn=10000+10;
-const int INF=0x6fffffff>>2;
- 
-struct edge{
-	int u,v,c,flow;
-};
- 
-struct Dinic{
-	int n,m,s,t;
-	vector<edge>edges;
-	vector<int>f[Maxn];
-	bool vis[Maxn];
-	int d[Maxn];
-	int cur[Maxn];
-	
-	void AddEdge(int from,int to,int c){
-		edges.push_back({from,to,c,0});
-		edges.push_back({to,from,0,0});
-		m=edges.size();
-		f[from].push_back(m-2);
-		f[to].push_back(m-1);
-	}
-	
-	bool BFS(){
-		memset(vis,0,sizeof(vis));
-		queue<int>q;
-		q.push(s);
-		d[s]=0;
-		vis[s]=1;
-		while(!q.empty()){
-			int x=q.front();q.pop();
-			for(int i=0;i<f[x].size();i++){
-				edge &e=edges[f[x][i]];
-				if(!vis[e.v]&&e.flow<e.c)
-				{
-					vis[e.v]=1;
-					d[e.v]=d[x]+1;
-					q.push(e.v);
-				}
-			}
-		}
-		return vis[t];
-	}
-	int DFS(int x,int a)
-	{
-		if(x==t||a==0)return a;
-		int flow=0,r;
-		for(int& i=cur[x];i<f[x].size();i++){
-			edge& e=edges[f[x][i]];
-			if(d[x]+1==d[e.v]&&(r=DFS(e.v,min(a,e.c-e.flow)))>0){
-				e.flow+=r;
-				edges[f[x][i]^1].flow-=r;
-				flow+=r;
-				a-=r;
-				if(a==0)break;
-			}
-		}
-		return flow;
-	}
-	int MaxFlow(int s,int t)
-	{
-		this->s=s;this->t=t;
-		int flow=0;
-		while(BFS())
-		{
-			memset(cur,0,sizeof(cur));
-			flow+=DFS(s,INF);
-		}
-		return flow;
-	}
-}G;
-int main()
-{
-	int x,y,z;
-	int s,t;
-	scanf("%d",&G.n);
-	while(1)
-	{
-		scanf("%d%d%d",&x,&y,&z);
-		if(!(x|y|z))break;
-		G.AddEdge(x,y,z);
-	}
-	scanf("%d%d",&s,&t);
-	printf("%d\n",G.MaxFlow(s,t));
-	return 0;
+const int N=10010,inf=0x3f3f3f3f;
+struct node{
+    int v,c,nxt;
+}e[N<<1];
+int tot=1,h[N],hp[N];
+void adde(int u,int v,int c){
+    e[++tot].v=v;
+    e[tot].c=c;
+    e[tot].nxt=h[u];
+    h[u]=tot;
+}
+int n,m,ans,dep[N];
+bool bfs(){
+    memset(dep,0,sizeof(dep));
+    queue<int>q;
+    while(!q.empty())q.pop();
+    dep[1]=1;
+    q.push(1);
+    do{
+        int u=q.front();
+        q.pop();
+        for(int i=h[u];i;i=e[i].nxt){
+            int v=e[i].v;
+            if(e[i].c&&!dep[v]){
+                dep[v]=dep[u]+1;
+                q.push(v);
+            }
+        }
+    }while(!q.empty());
+    return dep[n];
+}
+int dfs(int u,int f){
+    if(u==n)return f;
+    int ret=0;
+    for(int &i=hp[u];i;i=e[i].nxt){
+        int v=e[i].v;
+        if(e[i].c&&dep[v]==dep[u]+1){
+            int fv=dfs(v,min(f,e[i].c));
+            e[i].c-=fv;
+            e[i^1].c+=fv;
+            f-=fv;
+            ret+=fv;
+            if(!f)return ret;
+        }
+    }
+    return ret;
+}
+void solve(){
+    while(bfs()){
+        memcpy(hp,h,sizeof(h));
+        ans+=dfs(1,inf);
+    }
+}
+int main(){
+    cin>>n>>m;
+    for(int i=1;i<=m;i++){
+        int u,v,c;
+        cin>>u>>v>>c;
+        adde(u,v,c);
+        adde(v,u,0);
+    }
+    solve();
+    cout<<ans<<endl;
+    return 0;
 }
