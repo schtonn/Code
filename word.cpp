68499fd02a07349f00e8492c7083709358b36bce
schtonn <m18519511495@163.com>
2021-02-15 18:21:22 +0800

    everyday develepment

--- word.cpp
+++ word.cpp
@@ -1,25 +1,41 @@
 #include "bits/stdc++.h"
 using namespace std;
-int n,k,len;
-string s;
-int main(){
-    // freopen("word.in","r",stdin);
-    // freopen("word.out","w",stdout);
-    cin>>n>>k;
-    cin>>s;
-    cout<<s;
-    len+=s.length();
-    for(int i=1;i<n;i++){
-        cin>>s;
-        if(len+s.length()<=k){
-            cout<<' ';
-            cout<<s;
-            len+=s.length();
-        }else{
-            cout<<endl<<s;
-            len=s.length();
-        }
+int n;
+int exgcd(int a,int b,int &x,int &y){
+    if(b==0){
+        x=1;
+        y=0;
+        return a;
     }
-    system("pause");
+    int g=exgcd(b,a%b,y,x);
+    y-=(a/b)*x;
+    return g;
+}
+int main(){
+    int a,b,c,x=0,y=0,g;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a>>b>>c;
+        g=exgcd(a,b,x,y);
+        if(c%g){
+            cout<<"no solution"<<endl;
+            continue;
+        }
+        int w=c/g;
+        x*=w;
+        y*=w;
+        a/=g;
+        b/=g;
+        if(x>0){
+            int t=x/b;
+            x-=t*b;
+            y+=t*a;
+        }
+        while(x<=0){
+            x+=b;
+            y-=a;
+        }
+        cout<<x<<' '<<y<<endl;
+    }
     return 0;
 }
