6ca1efd2799c0087a7bc0dba8e858db015a58c75
schtonn <schtonn@163.com>
2021-02-18 22:07:35 +0800

    get started with xor gauss

--- hamar.cpp
+++ hamar.cpp
@@ -1,90 +1,39 @@
 #include "bits/stdc++.h"
 using namespace std;
+#define N 20
+double a[N][N];
 int n;
-__int128_t exgcd(__int128_t a,__int128_t b,__int128_t &x,__int128_t &y){
-    if(b==0){
-        x=1;
-        y=0;
-        return a;
-    }
-    __int128_t g=exgcd(b,a%b,y,x);
-    y-=(a/b)*x;
-    return g;
-}
-inline __int128_t read()
-{
-    __int128_t x=0,f=1;
-    char ch=getchar();
-    while(ch<'0'||ch>'9')
-    {
-        if(ch=='-')
-            f=-1;
-        ch=getchar();
-    }
-    while(ch>='0'&&ch<='9')
-    {
-        x=x*10+ch-'0';
-        ch=getchar();
-    }
-    return x*f;
-}
-
-inline void write(__int128_t x)
-{
-    if(x<0)
-    {
-        putchar('-');
-        x=-x;
-    }
-    if(x>9)
-        write(x/10);
-    putchar(x%10+'0');
-}
-bool modd(__int128_t a,__int128_t b){
-    // write(a);
-    // cout<<' ';
-    // write(b);
-    while(a>0){
-        a-=b;
-    }
-    if(a!=0)return true;
-    return false;
-}
 int main(){
-    __int128_t a,b,c,x=0,y=0,g;
     cin>>n;
     for(int i=0;i<n;i++){
-        a=read();
-        b=read();
-        c=read();
-        g=exgcd(a,b,x,y);
-        if(c%g){
-            cout<<"no solution"<<endl;
-            continue;
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
         }
-        __int128_t w=c/g;
-        x*=w;
-        y*=w;
-        a/=g;
-        b/=g;
-        if(x>0){
-            __int128_t t=x/b;
-            x-=t*b;
-            y+=t*a;
+    }
+    for(int i=0;i<n;i++){
+        cin>>a[i][n];
+    }
+    for(int i=0;i<n;i++){
+        int r=i;
+        for(int j=i;j<n;j++){
+            if(fabs(a[j][i]>fabs(a[r][i])))r=j;
         }
-        if(x<0){
-            __int128_t t=(-x)/b;
-            x+=t*b;
-            y-=t*a;
+        for(int j=0;j<=n;j++){
+            swap(a[r][j],a[i][j]);
         }
-        while(x<0){
-            x+=b;
-            y-=a;
+        for(int j=i+1;j<=n;j++){
+            a[i][j]/=a[i][i];
         }
-        write(x);
-        cout<<' ';
-        write(y);
-        cout<<endl;
+        for(int j=0;j<n;j++){
+            if(i!=j){
+                for(int k=i+1;k<=n;k++){
+                    a[j][k]-=a[j][i]*a[i][k];
+                }
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%.2lf\n",a[i][n]);
     }
     return 0;
 }
