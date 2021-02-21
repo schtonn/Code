0a924b28dcb02feecffebf56037fb5dafe34dad0
schtonn <schtonn@163.com>
2021-02-21 15:12:29 +0800

    T2

--- hamar.cpp
+++ hamar.cpp
@@ -1,43 +1,21 @@
 #include "bits/stdc++.h"
 using namespace std;
-const int N=1010,M=1010;
-int n,m,a[N],b[N],c[M],lef[N],sum[N],hsum[N],tsum[N];
-bool check(int rows){
-    for(int i=1;i<=n;i++){
-        if(b[i]-hsum[i]-sum[i]*rows<0)return false;
-    }
-    return true;
-}
-bool checktail(int rows){
-    for(int i=1;i<=n;i++){
-        if(lef[i]<0)return false;
-    }
-    return true;
-}
+const int N=100010,M=100010;
+int n,m,a[N],b[N],bb[N],c[M];
 int main(){
     cin>>n>>m;
     for(int i=1;i<=m;i++)cin>>b[i];
     for(int i=1;i<=n;i++){
         cin>>a[i]>>c[i];
-        sum[c[i]]+=a[i];
-        hsum[c[i]]=sum[c[i]];
     }
-    for(int i=1;i<=n;i++){
-        int l=0,r=N,mid;
-        while(l<=r){
-            mid=(l+r)/2;
-            if(check(mid))l=mid+1;
-            else r=mid-1;
+    for(int k=1;k<=n;k++){
+        memcpy(bb,b,sizeof(b));
+        int p=k-1;
+        while(bb[c[p%n+1]]>=a[p%n+1]){
+            bb[c[p%n+1]]-=a[p%n+1];
+            p++;
         }
-        for(int j=1;j<=m;j++)lef[j]=b[i]-hsum[i]-sum[i]*mid;
-        int tail=0;
-        while(checktail(tail)){
-            tail++;
-            lef[c[tail]]-=a[tail];
-        }
-        tail--;
-        cout<<n-i+mid*n+tail<<endl;
-        hsum[c[i]]-=a[i];
+        cout<<p-k+1<<' ';
     }
     return 0;
 }
