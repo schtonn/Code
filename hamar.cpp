93b908dbaef120d554106f9b163b627c8cb53939
schtonn <schtonn@163.com>
2021-03-27 14:34:18 +0800

    island

--- hamar.cpp
+++ hamar.cpp
@@ -1,11 +1,23 @@
 #include "bits/stdc++.h"
 using namespace std;
+const int N=100010;
+int n,q,a[N],b[N];
 int main(){
-    freopen("block.in","r",stdin);
-    freopen("block.out","w",stdout);
-    int n;
-    string a,b;
-    cin>>n>>a>>b;
-    cout<<n<<endl;
+    ios::sync_with_stdio(false);
+    freopen("island.in","r",stdin);
+    freopen("island.out","w",stdout);
+    cin>>n>>q;
+    for(int i=1;i<=n;i++){
+        cin>>a[i]>>b[i];
+    }
+    for(register int i=1;i<=q;i++){
+        int l,r,c,d;
+        cin>>l>>r>>c>>d;
+        int ans=0;
+        for(register int i=l;i<=r;i++){
+            if((a[i]^c)<=min(b[i],d))ans++;
+        }
+        cout<<ans<<endl;
+    }
     return 0;
 }
