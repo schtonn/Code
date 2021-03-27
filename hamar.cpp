858bd53cd0be1d67f68343bebc6a645e7682e950
schtonn <schtonn@163.com>
2021-03-27 18:39:29 +0800

    cake

--- hamar.cpp
+++ hamar.cpp
@@ -1,23 +1,46 @@
 #include "bits/stdc++.h"
 using namespace std;
-const int N=100010;
-int n,q,a[N],b[N];
+int gcd(int a,int b){
+    return b?gcd(b,a%b):a;
+}
 int main(){
-    ios::sync_with_stdio(false);
-    freopen("island.in","r",stdin);
-    freopen("island.out","w",stdout);
-    cin>>n>>q;
-    for(int i=1;i<=n;i++){
-        cin>>a[i]>>b[i];
-    }
-    for(register int i=1;i<=q;i++){
-        int l,r,c,d;
-        cin>>l>>r>>c>>d;
-        int ans=0;
-        for(register int i=l;i<=r;i++){
-            if((a[i]^c)<=min(b[i],d))ans++;
+    freopen("cake.in","r",stdin);
+    freopen("cake.out","w",stdout);
+    int T;
+    cin>>T;
+    while(T--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        int g=gcd(gcd(a,b),c);
+        a/=g;
+        b/=g;
+        c/=g;
+        if(a>b)swap(a,b);
+        if(b>c)swap(b,c); 
+        if(a>b)swap(a,b);
+        if(a==0&&b==0){
+            cout<<0<<endl;
+            continue;
         }
-        cout<<ans<<endl;
+        if(a==0){
+            if(b==c)cout<<1<<endl;
+            else cout<<2<<endl;
+            continue;
+        }
+        if(a+b==c||a==b||b==c){
+            cout<<2<<endl;
+            continue;
+        }
+        cout<<3<<endl;
     }
     return 0;
 }
+/*
+6
+0 0 8
+0 5 3
+9 9 0
+6 2 4
+1 7 4
+5 8 5
+*/
