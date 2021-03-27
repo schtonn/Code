87cc0a09e05b4cc16e7a93a805ade1b90105d392
schtonn <schtonn@163.com>
2021-03-27 18:40:08 +0800

    pacman

--- hamar.cpp
+++ hamar.cpp
@@ -1,83 +1,71 @@
 #include "bits/stdc++.h"
 using namespace std;
-const int N=260;
-int n,m,q,a[260][260];
-struct node{
-    int v,nxt;
-}e[N*N<<2];
-struct pos{
-    int x,y;
-}obs[N*N];
-int h[N*N<<2],tot;
-int p2i(int x,int y){
-    return x*(n+1)+y;
+const int N=1010;
+int n,a[N][N],l[N],u[N],r[N],d[N],id,mx,ans;
+inline void upd(int x){
+    l[x]=r[x]=u[x]=d[x]=0;
+    for(register int i=0;i<x-1;i++){
+        l[x]+=a[x-i][i+1];
+        r[x]+=a[n-x+1+i][n-i];
+    }
+    for(register int i=0;i<n-x;i++){
+        u[x]+=a[i+1][x+i];
+        d[x]+=a[n-i][n-x+1-i];
+    }
 }
-pos i2p(int k){
-    return pos{k/(n+1),k%(n+1)};
-}
-void adde(int u,int v){
-    if(u==v)return;
-    tot++;
-    e[tot].v=v;
-    e[tot].nxt=h[u];
-    h[u]=tot;
-}
-void ext(int x,int y){
-    if(!a[x-1][y])for(int i=x-2;!a[i][y]&&i>=0;i--){
-        adde(p2i(i,y),p2i(x-1,y));
+inline int getmax(){
+    mx=-1;
+    for(register int i=1;i<=n;i++){
+        int num=l[i]+u[i]+r[i]+d[i];
+        if(num>mx){
+            id=i;
+            mx=num;
+        }
     }
-    if(!a[x+1][y])for(int i=x+2;!a[i][y]&&i<=n;i++){
-        adde(p2i(i,y),p2i(x+1,y));
-    }
-    if(!a[x][y-1])for(int i=y-2;!a[x][i]&&i>=0;i--){
-        adde(p2i(x,i),p2i(x,y-1));
-    }
-    if(!a[x][y+1])for(int i=y+2;!a[x][i]&&i<=n;i++){
-        adde(p2i(x,i),p2i(x,y+1));
-    }
+    return mx;
 }
 int main(){
-    freopen("ball.in","r",stdin);
-    freopen("ball.out","w",stdout);
-    cin>>n>>m>>q;
-    for(int i=1;i<=m;i++){
-        int x,y;
-        cin>>x>>y;
-        obs[i].x=x;
-        obs[i].y=y;
-        a[obs[i].x][obs[i].y]=1;
+    ios::sync_with_stdio(false);
+    // freopen("pacman.in","r",stdin);
+    // freopen("pacman.out","w",stdout);
+    cin>>n;
+    for(register int i=1;i<=n;i++){
+        for(register int j=1;j<=n;j++){
+            cin>>a[i][j];
+        }
     }
-    for(int i=1;i<=n;i++){
-        obs[++m]=pos{0,i};
-        obs[++m]=pos{i,0};
-        obs[++m]=pos{n+1,i};
-        obs[++m]=pos{i,n+1};
-        a[0][i]=a[i][0]=a[n+1][i]=a[i][n+1]=1;
+    for(register int i=2;i<n;i++){
+        upd(i);
     }
-    for(int i=1;i<=m;i++){
-        ext(obs[i].x,obs[i].y);
+    for(register int i=1;i<=n;i++){
+        u[1]+=a[i][i];
+        l[n]+=a[n-i+1][i];
     }
-    int sx,sy,tx,ty;
-    for(int i=1;i<=q;i++){
-        cin>>sx>>sy>>tx>>ty;
-        /*
-        r98ccry23r8y938r939r8uc59t79eruxjeifnehdhuixjaluhu4rkwcr
-        3rw3
-        r
-        wrw
-        r
-        34rqwerw34tcw45
-        r
-        tw4q!!!!!!!!!!!!!!!!!!!3
-        cw5cwxd4qeiq9xjer8uq9fdhqi39
-        */
+    ans+=getmax();
+    for(register int i=0;i<id;i++){
+        a[id-i][i+1]=0;
+        a[n-id+1+i][n-i]=0;
     }
+    for(register int i=0;i<n-id;i++){
+        a[i+1][id+i]=0;
+        a[n-i][n-id+1-i]=0;
+    }
+    for(register int i=2;i<n;i++){
+        upd(i);
+    }
+    u[1]=l[n]=0;
+    for(register int i=1;i<=n;i++){
+        u[1]+=a[i][i];
+        l[n]+=a[n-i+1][i];
+    }
+    ans+=getmax();
+    cout<<ans<<endl;
     return 0;
 }
 /*
-4 4 3
-2 2
-2 4
-3 2
-4 4
+4
+20 1 19 2
+3 18 4 17
+16 5 15 6
+7 14 8 13
 */
