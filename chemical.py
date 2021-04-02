c3938d732a987204ec68551bf643347e9409a8bd
schtonn <schtonn@163.com>
2021-04-02 21:29:26 +0800

    chemical weich-check

--- chemical.py
+++ chemical.py
@@ -1,15 +1,39 @@
 import math
 
+eles=["H","He",
+    "Li","Be","B","C","N","O","F","Ne",
+    "Na","Mg","Al","Si","P","S","Cl","Ar",
+    "K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr",
+    "Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe",
+
+    "Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Hf",
+    "Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn",
+
+    "Fr","Ra","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr","Rf",
+    "Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"
+]
+
 priv=[2,2,
     1,1,2,2,2,3,2,2,
     1,1,1,2,2,2,2,2,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2
 ]
 
+weigh=[0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+]
+err=0
+
 def getc(s,pos):
+    # print("getc",s,pos)
     s=s[pos:len(s)]
     ret=1
     for i in range(len(s)):
@@ -17,32 +41,66 @@ def getc(s,pos):
         if(not s[0:i+1].isnumeric()):break
     return ret
 
+def ele(s,coef):
+    # print("ele",s,coef)
+    global weigh
+    for i in eles:
+        if(s==i):
+            weigh[eles.index(i)]+=coef
+            return
+    global err
+    err=1
+
 def term(s,coef):
-    print(s,coef)
+    # print("term",s,coef)
     i=0
     while i<len(s):
         if(s[i]=='('):
             sub=s.rfind(')')
             ncoef=getc(s,sub+1)
             pre=int(math.log10(ncoef))
+            if(sub<len(s)-1 and (not s[sub+1].isdigit())):pre=0
             term(s[i+1:sub-pre],coef*ncoef)
             i=sub
-        else:
+        elif(s[i].isupper()):
             nele=s[i]
-            if(s[i+1].isalpha()):nele+=s[i+1]
-            
+            if(i<len(s)-1 and s[i+1].islower()):
+                nele+=s[i+1]
+                i+=1
+            ncoef=getc(s,i+1)
+            pre=int(math.log10(ncoef))
+            if(i<len(s)-1 and (not s[i+1].isdigit())):pre=0
+            ele(nele,coef*ncoef)
+            i+=pre
         i+=1
 
-def form(s):
-    coef=getc(s,0)
-    pre=int(math.log10(coef)+1)
-    term(s[pre:len(s)],coef)
+def form(s,coef):
+    # print("form",s,coef)
+    ncoef=getc(s,0)
+    pre=int(math.log10(ncoef)+1)
+    if(not s[0].isdigit()):pre=0
+    term(s[pre:len(s)],coef*ncoef)
 
-def expr(s):
+def expr(s,coef):
+    # print("expr",s,coef)
     forms=s.split('+')
-    for i in forms:form(i)
+    for i in forms:form(i,coef)
 
-s=input()
-lr=s.split('=',1)
-expr(lr[0])
-expr(lr[1])
+def printt():
+    for i in range(len(weigh)):
+        if(weigh[i]!=0):print(eles[i],':',weigh[i],sep="")
+
+def init():
+    global weigh
+    for i in range(len(weigh)):weigh[i]=0
+    global err
+    err=0
+
+while 1:
+    init()
+    s=input()
+    lr=s.split('=',1)
+    expr(lr[0],1)
+    expr(lr[1],-1)
+    if(err==1):print("err!")
+    else:printt()
