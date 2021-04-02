960bff8b612f40f95e431244b4cc9586fe856229
schtonn <schtonn@163.com>
2021-04-02 20:25:32 +0800

    chemical term

--- chemical.py
+++ chemical.py
@@ -13,23 +13,29 @@ def getc(s,pos):
     s=s[pos:len(s)]
     ret=1
     for i in range(len(s)):
+        if(s[0:i+1].isnumeric()):ret=int(s[0:i+1])
         if(not s[0:i+1].isnumeric()):break
     return ret
 
-
 def term(s,coef):
+    print(s,coef)
     i=0
     while i<len(s):
         if(s[i]=='('):
             sub=s.rfind(')')
-            ncoef=1
-            term(s[i+1:sub],coef*ncoef)
+            ncoef=getc(s,sub+1)
+            pre=int(math.log10(ncoef))
+            term(s[i+1:sub-pre],coef*ncoef)
             i=sub
+        else:
+            nele=s[i]
+            if(s[i+1].isalpha()):nele+=s[i+1]
+            
         i+=1
 
 def form(s):
     coef=getc(s,0)
-    pre=math.log10(coef)+1
+    pre=int(math.log10(coef)+1)
     term(s[pre:len(s)],coef)
 
 def expr(s):
