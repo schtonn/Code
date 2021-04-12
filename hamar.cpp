9b12e68d6e9344668e3c76da0965955f0c017bdc
schtonn <schtonn@163.com>
2021-04-12 18:28:54 +0800

    rabbit farm inv

--- hamar.cpp
+++ hamar.cpp
@@ -1,35 +1,30 @@
 #include "bits/stdc++.h"
 using namespace std;
-const long long N=1000010;
-long long m,n,k,p;
+typedef long long ll;
+const ll N=1000010;
+ll m,n,k,p;
 struct matrix{
-    long long v[5][5];
-    long long x,y;
+    ll v[5][5];
+    ll x,y;
 }f,A,B,I,ans;
-long long e[N],g[N];
-void geteg(long long u){
-    long long a=u,b=u*2%k,st=1;
-    while(b!=1){
-        st++;
-        if((a==0&&b==u)||(a==b&&e[a]==-1)){
-            st=a=-1;
-            break;
-        }
-        if(a==b&&e[a]){
-            st+=e[a]-1;
-            a=g[a];
-            break;
-        }
-        // cout<<"("<<a<<","<<b<<")->";
-        swap(a,b);
-        b=(a+b)%k;
+ll e[N],g[N],inv[N];
+void exgcd(ll a,ll b,ll& d,ll& x,ll& y){
+    if(!b){
+        d=a;
+        x=1;y=0;
+        return;
     }
-    e[u]=st;
-    g[u]=a;
+    exgcd(b,a%b,d,y,x);
+    y-=x*(a/b);
+}
+ll getinv(ll a){
+    ll d,x,y;
+    exgcd(a,k,d,x,y);
+    return d==1?(x+k)%k:-1;
 }
 ostream&operator<<(ostream&ous,matrix a){
-    for(long long i=0;i<a.x;i++){
-        for(long long j=0;j<a.y;j++){
+    for(ll i=0;i<a.x;i++){
+        for(ll j=0;j<a.y;j++){
             ous<<a.v[i][j]<<' ';
         }
         ous<<endl;
@@ -39,11 +34,11 @@ ostream&operator<<(ostream&ous,matrix a){
 matrix operator+(matrix a,matrix b){
     matrix c;
     if(a.x!=b.x||a.y!=b.y)return c;
-    long long x=a.x,y=a.y;
+    ll x=a.x,y=a.y;
     c.x=x;
     c.y=y;
-    for(long long i=0;i<x;i++){
-        for(long long j=0;j<y;j++){
+    for(ll i=0;i<x;i++){
+        for(ll j=0;j<y;j++){
             c.v[i][j]=(a.v[i][j]+b.v[i][j])%p;
         }
     }
@@ -52,20 +47,20 @@ matrix operator+(matrix a,matrix b){
 matrix operator*(matrix a,matrix b){
     matrix c;
     if(a.y!=b.x)return c;
-    long long x=a.x,y=b.y,z=a.y;
+    ll x=a.x,y=b.y,z=a.y;
     c.x=x;
     c.y=y;
-    for(long long i=0;i<x;i++){
-        for(long long j=0;j<y;j++){
+    for(ll i=0;i<x;i++){
+        for(ll j=0;j<y;j++){
             c.v[i][j]=0;
-            for(long long k=0;k<z;k++){
+            for(ll k=0;k<z;k++){
                 c.v[i][j]=(c.v[i][j]+a.v[i][k]*b.v[k][j])%p;
             }
         }
     }
     return c;
 }
-matrix operator^(matrix a,long long b){
+matrix operator^(matrix a,ll b){
     matrix c=I;
     while(b){
         if(b&1)c=c*a;
@@ -89,24 +84,33 @@ int main(){
     // freopen("rabbit.out","w",stdout);
     init();
     cin>>n>>k>>p;
-    for(long long i=1;i<k;i++){
-        geteg(i);
-       if(e[i]==-1)cout<<"N"<<endl;
-       else cout<<"Y"<<endl;
+    int a=1,b=2;
+    memset(e,-1,sizeof(e));
+    for(int i=1;i<=k*k;i++){
+        if(a==1&&b==1)break;
+        if(!inv[b])inv[b]=getinv(b);
+        if(inv[b]==-1){
+            swap(a,b);
+            b=(a+b)%k;
+            continue;
+        }
+        if(e[inv[b]]==-1){
+            e[inv[b]]=i;
+            g[inv[b]]=a*inv[b]%k;
+        }
+        swap(a,b);
+        b=(a+b)%k;
     }
-    long long cur=1,ptr=0;
+    ll cur=1,ptr=0;
     while(true){
         if(ptr+e[cur]+1>n||e[cur]==-1){
             f=(A^(n-ptr))*f;
             break;
         }
-        // cout<<"a\n"<<f;
         f=(A^(e[cur]+1))*f;
-        // cout<<"b\n"<<f;
         ptr+=e[cur]+1;
         if(ptr>=n)break;
         f=B*f;
-        // cout<<"c\n"<<f;
         ptr++;
         cur=g[cur];
         if(ptr>=n)break;
