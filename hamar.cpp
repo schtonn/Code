2216877749523ac4f3a245d2a30c4cdc7e0ed54d
schtonn <schtonn@163.com>
2021-04-17 18:17:36 +0800

    python and contest

--- hamar.cpp
+++ hamar.cpp
@@ -1,153 +1,67 @@
 #include "bits/stdc++.h"
 using namespace std;
-typedef long long ll;
-const ll N=1000010;
-ll m,n,k,p;
-struct matrix{
-    ll v[5][5];
-    ll x,y;
-}f,A,B,I,ans;
-ll e[N],g[N],inv[N],vis[N];
-void exgcd(ll a,ll b,ll& d,ll& x,ll& y){
-    if(!b){
-        d=a;
-        x=1;y=0;
-        return;
-    }
-    exgcd(b,a%b,d,y,x);
-    y-=x*(a/b);
-}
-ll getinv(ll a){
-    ll d,x,y;
-    exgcd(a,k,d,x,y);
-    return d==1?(x+k)%k:-1;
-}
-ostream&operator<<(ostream&ous,matrix a){
-    for(ll i=0;i<a.x;i++){
-        for(ll j=0;j<a.y;j++){
-            ous<<a.v[i][j]<<' ';
-        }
-        ous<<endl;
-    }
-    return ous;
-}
-matrix operator+(matrix a,matrix b){
-    matrix c;
-    if(a.x!=b.x||a.y!=b.y)return c;
-    ll x=a.x,y=a.y;
-    c.x=x;
-    c.y=y;
-    for(ll i=0;i<x;i++){
-        for(ll j=0;j<y;j++){
-            c.v[i][j]=(a.v[i][j]+b.v[i][j])%p;
+const long long N=500010;
+long long n,a[N][5],siz[5][2]={{2,2},{4,1},{1,1},{2,2},{2,2}},shp[5][4][2]={
+    {{1,1},{1,1}},{{1},{1},{1},{1},},{{1}},{{1,1},{1}},{{0,1},{1,1}}
+},top=0;
+void clear(long long u){
+    for(register long long i=u;i<=top;i++){
+        for(register long long j=0;j<5;j++){
+            a[i][j]=a[i+1][j];
         }
     }
-    return c;
+    top--;
 }
-matrix operator*(matrix a,matrix b){
-    matrix c;
-    if(a.y!=b.x)return c;
-    ll x=a.x,y=b.y,z=a.y;
-    c.x=x;
-    c.y=y;
-    for(ll i=0;i<x;i++){
-        for(ll j=0;j<y;j++){
-            c.v[i][j]=0;
-            for(ll k=0;k<z;k++){
-                c.v[i][j]=(c.v[i][j]+a.v[i][k]*b.v[k][j])%p;
-            }
-        }
-    }
-    return c;
-}
-matrix operator^(matrix a,ll b){
-    matrix c=I;
-    while(b){
-        if(b&1)c=c*a;
-        a=a*a;
-        b>>=1;
-    }
-    return c;
-}
-void init(){
-    A.x=A.y=B.x=B.y=3;
-    I.x=I.y=3;
-    f.x=3;f.y=1;
-    f.v[1][0]=f.v[2][0]=1;
-    A.v[0][0]=A.v[0][1]=A.v[1][0]=A.v[2][2]=1;
-    B.v[0][0]=B.v[0][1]=B.v[1][0]=B.v[2][2]=1;
-    B.v[0][2]=-1;
-    I.v[0][0]=I.v[1][1]=I.v[2][2]=1;
-}
-int main(){
-    // freopen("rabbit.in","r",stdin);
-    // freopen("rabbit.out","w",stdout);
-    init();
-    cin>>n>>k>>p;
-    int a=1,b=2;
-    memset(e,-1,sizeof(e));
-    for(int i=1;i<=k*k;i++){
-        if(a==1&&b==1)break;
-        if(!inv[b])inv[b]=getinv(b);
-        if(inv[b]==-1){
-            swap(a,b);
-            b=(a+b)%k;
-            continue;
-        }
-        if(e[inv[b]]==-1){
-            e[inv[b]]=i;
-            g[inv[b]]=a*inv[b]%k;
-        }
-        swap(a,b);
-        b=(a+b)%k;
-    }
-    ll flag=0;
-    ll cur=1,ptr=0;
-    while(true){
-        if(vis[cur]){
-            flag=cur;
-            break;
-        }
-        vis[cur]=1;
-        if(ptr+e[cur]+1>n||e[cur]==-1){
-            f=(A^(n-ptr))*f;
-            break;
-        }
-        f=(A^(e[cur]+1))*f;
-        ptr+=e[cur]+1;
-        if(ptr>=n)break;
-        f=B*f;
-        ptr++;
-        cur=g[cur];
-        if(ptr>=n)break;
-    }
-    matrix C;
-    ll siz;
-    if(flag){
-        siz=e[flag]+2;
-        C=B*(A^(e[flag]+1));
-        for(ll i=g[flag];i!=flag;i=g[i]){
-            C=B*(A^(e[i]+1))*C;
-            siz+=e[i]+2;
-        }
-        f=(C^((n-ptr)/siz))*f;
-        if((n-ptr)%siz){
-            ptr=n-((n-ptr)%siz);
-            while(true){
-                if(ptr+e[cur]+1>n){
-                    f=(A^(n-ptr))*f;
+void put(long long t,long long l,long long c){
+    long long p=0;
+    for(p=top+10;p>=0;p--){
+        long long flag=0;
+        for(register long long i=0;i<siz[t][0];i++){
+            for(register long long j=0;j<siz[t][1];j++){
+                if(shp[t][i][j]&&a[p+i][l+j]!=-1){
+                    flag=1;
                     break;
                 }
-                f=(A^(e[cur]+1))*f;
-                ptr+=e[cur]+1;
-                if(ptr>=n)break;
-                f=B*f;
-                ptr++;
-                cur=g[cur];
-                if(ptr>=n)break;
             }
+            if(flag)break;
         }
-    } 
-    cout<<(f.v[0][0]+p)%p<<endl;
+        flag=flag;
+        if(flag)break;
+    }
+    p++;
+    top=max(siz[t][0]+p-1,top);
+    for(register long long i=0;i<siz[t][0];i++){
+        for(register long long j=0;j<siz[t][1];j++){
+            if(shp[t][i][j])a[p+i][l+j]=c;
+        }
+    }
+    for(register long long i=top;i>=0;i--){
+        long long flag=0;
+        for(register long long j=0;j<5;j++){
+            if(a[i][j]==-1)flag=1;
+        }
+        if(!flag)clear(i);
+    }
+}
+void print(){
+    for(register long long i=top;i>=0;i--){
+        for(register long long j=0;j<5;j++){
+            if(a[i][j]==-1)cout<<" ";
+            else cout<<a[i][j];
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin>>n;
+    memset(a,-1,sizeof(a));
+    for(register long long i=1;i<=n;i++){
+        long long t,l,c;
+        cin>>t>>l>>c;
+        put(t-1,l-1,c);
+    }
+    print();
     return 0;
 }
---
+++ ss.py
@@ -0,0 +1,52 @@
+# 网易云 根据歌单链接下载MP3歌曲
+import requests
+from bs4 import BeautifulSoup
+import re
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
+
+def main():
+    url = "https://music.163.com/#/playlist?id=5198659099" # 歌单地址 请自行更换
+    if '/#/' in url:
+        url = url.replace('/#/', '/')
+
+    s = requests.session()
+    try:
+        response = s.get(url, headers=headers).content
+        soup = BeautifulSoup(response, 'lxml')
+        lis = list(soup.find('ul'))
+        fatherlis = ['歌单名：' + str(soup.find('h2').string)]
+        for i in lis:
+            sonlis = []
+            sonlis.append(str(len(fatherlis)) + '.')
+            sonlis.append(i.a.string)
+            sonlis.append(str(i.a.get('href'))[str(i.a.get('href')).find('=') + 1:-1] + str(i.a.get('href'))[-1])
+            fatherlis.append(sonlis)
+    except:
+        print("\n\t歌曲链接输入错误")
+        exit('ERROR!')
+    format = '{0:<10}\t{1:{3}<10}\t{2:<10}'
+    print("从'{}'中找到了{}条歌曲".format(str(soup.find('h2').string), len(fatherlis) - 1))
+    print('-------------------------------------------------------------------------------------------------')
+    print('序号         歌曲名称        歌曲链接')
+    for i in fatherlis:
+        if fatherlis.index(i) == 0:
+            continue
+        else:
+            print(
+                format.format(i[0], i[1], 'http://music.163.com/song/media/outer/url?id=' + i[2] + '.mp3', chr(12288)))
+            download_music(i[2],i[1])
+    print('##########################下载完成##########################')
+
+# 歌曲下载
+def download_music(song_id,song_name):
+    song_name=re.sub("[\\/:\\*\\?\"<>|]","",song_name)
+    out=requests.get("http://music.163.com/song/media/outer/url?id="+song_id+".mp3",headers=headers,timeout=20)
+    mus=out.content
+    print("good")
+    with open(r"d:/Downloads/Music/"+song_name+".mp3",'ab') as file:
+        file.write(mus)
+        file.flush()
+
+if __name__ == '__main__':
+    main()
