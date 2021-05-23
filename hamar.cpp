b5263c016fbe22893b9a0f210f92f2e82811d91a
schtonn <schtonn@163.com>
2021-05-23 08:26:04 +0800

    exgcd

--- hamar.cpp
+++ hamar.cpp
@@ -1,46 +1,48 @@
 #include "bits/stdc++.h"
 using namespace std;
-const int N=100010,M=100010;
-struct node{
-    int v,nxt;
-}e[N<<2];
-int n,m,h[N],in[N],st[N],tot;
-void adde(int u,int v){
-    tot++;
-    e[tot].v=v;
-    e[tot].nxt=h[u];
-    h[u]=tot;
+typedef long long ll;
+ll n;
+ll exgcd(ll a,ll b,ll &x,ll &y){
+    if(b==0){
+        x=1;
+        y=0;
+        return a;
+    }
+    ll g=exgcd(b,a%b,y,x);
+    y-=(a/b)*x;
+    return g;
 }
 int main(){
-    cin>>n>>m;
-    for(int i=0;i<m;i++){
-        int u,v;
-        cin>>u>>v;
-        adde(u,v);
-        in[v]++;
-    }
-    priority_queue<int,vector<int>,greater<int> >q;
-    for(int i=1;i<=n;i++){
-        if(!in[i])q.push(i);
-    }
-    int cnt=0;
-    while(!q.empty()){
-        int u=q.top();
-        q.pop();
-        for(int i=h[u];i;i=e[i].nxt){
-            int v=e[i].v;
-            in[v]--;
-            if(!in[v])q.push(v);
+    freopen("file2.out","w",stdout);
+    ll a,b,c,x=0,y=0,g;
+    cin>>n;
+    for(ll i=0;i<n;i++){
+        cin>>a>>b>>c;
+        g=exgcd(a,b,x,y);
+        if(c%g){
+            cout<<"no solution"<<endl;
+            continue;
         }
-        h[u]=0;
-        st[cnt]=u;
-        cnt++;
-    }
-    if(cnt!=n)cout<<"No"<<endl;
-    else{
-        for(int i=0;i<n;i++){
-            cout<<st[i]<<' ';
+        ll w=c/g;
+        x*=w;
+        y*=w;
+        a/=g;
+        b/=g;
+        if(x>0){
+            ll t=x/b;
+            x-=t*b;
+            y+=t*a;
         }
+        if(x<0){
+            ll t=(-x)/b;
+            x+=t*b;
+            y-=t*a;
+        }
+        while(x<0){
+            x+=b;
+            y-=a;
+        }
+        cout<<x<<' '<<y<<endl;
     }
     return 0;
 }
