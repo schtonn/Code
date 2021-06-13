e98ba7915e89aa7283d4134f54c025a42374b89e
schtonn <schtonn@163.com>
2021-06-13 20:09:57 +0800

    xor change

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,19 @@
 #include "bits/stdc++.h"
 using namespace std;
+char s[100010];
 int main(){
-	int a;
-	return 0;
+    long long n,t;
+    cin>>n>>t>>s;
+    long long len=n;
+    for(long long i=0;i<n&&s[i]=='0';i++)len--;
+    long long c=1;
+    while(c<len)c<<=1;
+    t%=c;
+    for(int j=1;j<=t;j++){
+        for(int i=n-1;i>=1;i--){
+            s[i]=(s[i]^s[i-1])?'1':'0';
+        }
+    }
+    cout<<s;
+    return 0;
 }
