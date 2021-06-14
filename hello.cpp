d59352b640e70090599a6204595dae06e6c29e1f
schtonn <schtonn@163.com>
2021-06-14 08:44:46 +0800

    iceberg multiInsert

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,10 @@
 #include "bits/stdc++.h"
 using namespace std;
 typedef long long ll;
-const ll inf=114514,N=100010,mod=998244353;
+const ll inf=0x3f3f3f3f3f3f3f3f,N=100010,mod=998244353;
 ll son[N][2],v[N],rnd[N],s[N],cnt[N],root,t,sum,B;
 ll brand(){
-	return rand()<<16|rand();	
+    return rand()<<16|rand();
 }
 void update(ll p){
     s[p]=s[son[p][0]]+s[son[p][1]]+cnt[p];
@@ -28,6 +28,7 @@ void insert(ll &p,ll vp){
     }
     if(v[p]==vp){
         cnt[p]++;
+        update(p);
         return;
     }
     s[p]++;
@@ -35,11 +36,31 @@ void insert(ll &p,ll vp){
     insert(son[p][op],vp);
     if(rnd[son[p][op]]>rnd[p])rotate(p,!op);
 }
+void multiInsert(ll &p,ll vp,ll num){
+    if(!p){
+        p=++t;
+        v[p]=vp;
+        rnd[p]=brand();
+        s[p]=1;
+        cnt[p]=num;
+        return;
+    }
+    if(v[p]==vp){
+        cnt[p]+=num;
+        update(p);
+        return;
+    }
+    s[p]++;
+    bool op=vp>v[p];
+    multiInsert(son[p][op],vp,num);
+    if(rnd[son[p][op]]>rnd[p])rotate(p,!op);
+}
 void del(ll &p,ll vp){
     if(!p)return;
     if(vp==v[p]){
         if(cnt[p]>1){
             cnt[p]--;
+            update(p);
             return;
         }
         if(son[p][0]&&son[p][1]){
@@ -59,13 +80,13 @@ ll pre(ll p,ll vp){
     if(vp>v[p])return max(pre(son[p][1],vp),v[p]);
     else return pre(son[p][0],vp);
 }
-ll erp(ll p,ll vp){
+ll nxt(ll p,ll vp){
     if(!p)return inf;
-    if(vp<v[p])return min(erp(son[p][0],vp),v[p]);
-    else return erp(son[p][1],vp);
+    if(vp<v[p])return min(nxt(son[p][0],vp),v[p]);
+    else return nxt(son[p][1],vp);
 }
 int main(){
-    srand(114514);
+    srand(20210613);
     ll n,m,k,rt=0;
     cin>>n>>m>>k;
     for(ll i=1;i<=n;i++){
@@ -78,21 +99,21 @@ int main(){
         ll x,y;
         cin>>x>>y;
         B-=x;
-        sum=(sum+x*n)%mod;
+        sum=((sum+x*n)+mod)%mod;
         ll p=pre(rt,B+1);
         while(p!=-inf){
             del(rt,p);
             n--;
-            sum=(sum-p+B+mod)%mod;
+            sum=((sum-p+B)+mod)%mod;
             p=pre(rt,B+1);
         }
-        p=erp(rt,k+B);
+        p=nxt(rt,k+B);
         while(p!=inf){
             del(rt,p);
             insert(rt,k+B);
-            for(ll i=0;i<p-k-B;i++)insert(rt,1+B);
+            multiInsert(rt,1+B,p-k-B);
             n+=p-k-B;
-            p=erp(rt,k+B);
+            p=nxt(rt,k+B);
         }
         if(y){
             insert(rt,y+B);
